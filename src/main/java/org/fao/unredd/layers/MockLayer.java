38f192547ef16f9a06486a639fa2d5717c6feb99
Fernando Gonzalez Cortes <fernando.gonzalez@geomati.co>
2013-05-30 15:55:41 +0200

    Added dependencyManagement in pom.xml. JSON generated manually for Indicator. Service to return (mocked) indicator content.

--- src/main/java/org/fao/unredd/layers/MockLayer.java
+++ src/main/java/org/fao/unredd/layers/MockLayer.java
@@ -2,9 +2,17 @@ package org.fao.unredd.layers;
 
 public class MockLayer implements Layer {
 
+	private Indicator indicator = new Indicator("zonal-stats", "Statistics",
+			"text/html", "These are the statistics maaaaan");
+
 	@Override
-	public Indicator[] getIndicators() {
-		return new Indicator[] { new Indicator("zonal-stats", "Statistics") };
+	public Indicators getIndicators() {
+		return new Indicators(indicator);
+	}
+
+	@Override
+	public Indicator getIndicator(String indicatorId) {
+		return indicator;
 	}
 
 }
