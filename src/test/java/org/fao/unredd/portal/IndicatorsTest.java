bca6d70e903643827afc25963fc86e489ddbc630
Fernando Gonzalez Cortes <fernando.gonzalez@geomati.co>
2013-05-31 11:24:08 +0200

    Extracted service to return concrete indicator

--- src/test/java/org/fao/unredd/portal/IndicatorsTest.java
+++ src/test/java/org/fao/unredd/portal/IndicatorsTest.java
@@ -1,15 +1,14 @@
 package org.fao.unredd.portal;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.argThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
@@ -19,6 +18,9 @@ import org.fao.unredd.layers.Indicator;
 import org.fao.unredd.layers.Indicators;
 import org.fao.unredd.layers.Layer;
 import org.fao.unredd.layers.LayerFactory;
+import org.fao.unredd.layers.NoSuchIndicatorException;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
@@ -30,25 +32,37 @@ public class IndicatorsTest {
 	private HttpServletResponse response;
 	private LayerFactory layerFactory;
 	private StringWriter responseWriter;
+	private IndicatorsController indicators;
 
 	@Before
-	public void setupCommon() throws IOException {
+	public void setupCommon() throws Exception {
 		response = mock(HttpServletResponse.class);
 		responseWriter = new StringWriter();
 		when(response.getWriter()).thenReturn(new PrintWriter(responseWriter));
 
 		Layer layer = mock(Layer.class);
-		when(layer.getIndicators()).thenReturn(
-				new Indicators(new Indicator("id", "name", CONTENT_TYPE,
-						CONTENT)));
+		Indicator indicator = new Indicator("id", "name", CONTENT_TYPE, CONTENT);
+		when(layer.getIndicators()).thenReturn(new Indicators(indicator));
+		when(layer.getIndicator("id")).thenReturn(indicator);
+		when(layer.getIndicator(argThat(new BaseMatcher<String>() {
+
+			@Override
+			public boolean matches(Object argument) {
+				return !argument.equals("id");
+			}
+
+			@Override
+			public void describeTo(Description description) {
+				description.appendText("Different from id");
+			}
+		}))).thenThrow(new NoSuchIndicatorException());
 		layerFactory = mock(LayerFactory.class);
 		when(layerFactory.newLayer(anyString())).thenReturn(layer);
+		indicators = new IndicatorsController(response, layerFactory);
 	}
 
 	@Test
-	public void testOk() throws Exception {
-		IndicatorsController indicators = new IndicatorsController(response,
-				layerFactory);
+	public void testIndicatorsOk() throws Exception {
 		indicators.returnIndicators("thelayerid");
 
 		verify(response, never()).setStatus(anyInt());
@@ -61,14 +75,40 @@ public class IndicatorsTest {
 	}
 
 	@Test
-	public void testNullParameters() throws Exception {
-		IndicatorsController indicators = new IndicatorsController(response,
-				layerFactory);
-		try {
-			indicators.returnIndicators(null);
-			fail();
-		} catch (NullPointerException e) {
-		}
+	public void testIndicatorOk() throws Exception {
+		indicators.returnIndicator("thelayerid", "id");
+
+		verify(response, never()).setStatus(anyInt());
+		verify(response).setContentType(CONTENT_TYPE);
+		assertTrue(responseWriter.getBuffer().toString().equals(CONTENT));
+	}
+
+	@Test
+	public void testIndicatorNotFound() throws Exception {
+		indicators.returnIndicator("thelayerid", "notfound");
+		verify(response).setStatus(
+				ApplicationController.ErrorCause.ILLEGAL_ARGUMENT.getStatus());
+	}
+
+	@Test
+	public void testIndicatorsNullLayerId() throws Exception {
+		indicators.returnIndicators(null);
+		verify(response).setStatus(
+				ApplicationController.ErrorCause.ILLEGAL_ARGUMENT.getStatus());
+	}
+
+	@Test
+	public void testIndicatorNullLayerId() throws Exception {
+		indicators.returnIndicator(null, "indicatorId");
+		verify(response).setStatus(
+				ApplicationController.ErrorCause.ILLEGAL_ARGUMENT.getStatus());
+	}
+
+	@Test
+	public void testIndicatorNullIndicatorId() throws Exception {
+		indicators.returnIndicator("layerId", null);
+		verify(response).setStatus(
+				ApplicationController.ErrorCause.ILLEGAL_ARGUMENT.getStatus());
 	}
 
 }
