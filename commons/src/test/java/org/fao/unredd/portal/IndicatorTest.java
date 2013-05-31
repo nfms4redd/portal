8b92fbb34cfc52e4fbaefd6ba743c04b0d66509e
Fernando Gonzalez Cortes <fernando.gonzalez@geomati.co>
2013-05-31 10:20:43 +0200

    minor change: renaming test

--- commons/src/test/java/org/fao/unredd/portal/IndicatorTest.java
+++ commons/src/test/java/org/fao/unredd/portal/IndicatorTest.java
@@ -14,14 +14,12 @@ import org.junit.Test;
 public class IndicatorTest {
 
 	@Test
-	public void testExpectedJSONSerialization() throws Exception {
+	public void testSerialization() throws Exception {
 		Indicators indicators = new Indicators(new Indicator("a", "b",
 				"application/json", "[{}]"));
 
 		ObjectMapper mapper = new ObjectMapper();
-		String json = indicators.toJSON();
-		System.out.println(json);
-		JsonNode tree = mapper.readTree(json);
+		JsonNode tree = mapper.readTree(indicators.toJSON());
 		assertEquals(1, tree.size());
 		JsonNode indicatorNode = tree.get(0);
 		assertEquals("a", indicatorNode.get("id").asText());
