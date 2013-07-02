1f64b369f060f16279a0dbc3b6c0b012b41029e9
Fernando Gonzalez Cortes <fernando.gonzalez@geomati.co>
2013-07-02 11:01:15 +0200

    added license headers

--- commons/src/main/java/org/fao/unredd/layers/folder/FolderLayerFactory.java
+++ commons/src/main/java/org/fao/unredd/layers/folder/FolderLayerFactory.java
@@ -1,3 +1,18 @@
+/**
+ * nfms4redd Portal Interface - http://nfms4redd.org/
+ *
+ * (C) 2012, FAO Forestry Department (http://www.fao.org/forestry/)
+ *
+ * This application is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation;
+ * version 3.0 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ */
 package org.fao.unredd.layers.folder;
 
 import java.io.File;
