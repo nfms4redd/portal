e2ad35abb1313471b5423fc83b727e216f5875ed
Fernando Gonzalez Cortes <fernando.gonzalez@geomati.co>
2013-10-09 15:53:40 +0200

    more properties of layers.json taken into account

--- src/main/webapp/modules/main.js
+++ src/main/webapp/modules/main.js
@@ -20,63 +20,75 @@ require([ "jquery" ], function($) {
 		}
 	};
 
+	var findById = function(array, id) {
+		var matches = $.grep(array, function(l) {
+			return l.id == id;
+		});
+		if (matches.length == 1) {
+			return matches[0];
+		} else {
+			return null;
+		}
+	};
+
 	require([ "customization" ]);
 
-	$(document).bind(
-			"customization-received",
-			function(event, customization) {
+	$(document).bind("customization-received", function(event, customization) {
 
-				var languageCode = customization.languageCode;
-				require([ "jquery", "iso8601", "css-loader", "layout", "error-management" ], function($) {
-					$(document).trigger("css-load", "styles/jquery-ui-1.8.16.custom.css");
-					$(document).trigger("css-load", "styles/jquery.fancybox.css");
+		var languageCode = customization.languageCode;
+		require([ "jquery", "iso8601", "css-loader", "layout", "error-management" ], function($) {
+			$(document).trigger("css-load", "styles/jquery-ui-1.8.16.custom.css");
+			$(document).trigger("css-load", "styles/jquery.fancybox.css");
 
-					$.ajax({
-						dataType : "json",
-						url : "layers",
-						data : "lang=" + languageCode,
-						success : function(data, textStatus, jqXHR) {
-							var groups = data.groups;
-							for (var i = 0; i < groups.length; i++) {
-								var group = groups[i];
-								$(document).trigger("add-group", {
-									"id" : group.id,
-									"name" : group.label
-								});
+			$.ajax({
+				dataType : "json",
+				url : "layers",
+				data : "lang=" + languageCode,
+				success : function(data, textStatus, jqXHR) {
+					var groups = data.groups;
+					for (var i = 0; i < groups.length; i++) {
+						var group = groups[i];
+						$(document).trigger("add-group", {
+							"id" : group.id,
+							"name" : group.label
+						});
 
-								var items = group.items;
-								for (var j = 0; j < items.length; j++) {
-									var matches = $.grep(data.portalLayers, function(l) {
-										return l.id == items[j];
+						var items = group.items;
+						for (var j = 0; j < items.length; j++) {
+							var portalLayer = findById(data.portalLayers, items[j]);
+							if (portalLayer != null) {
+								var wmsLayer = findById(data.wmsLayers, portalLayer.layers[0]);
+								if (wmsLayer != null) {
+									var url = wmsLayer.baseUrl;
+									var wmsName = wmsLayer.wmsName;
+									$(document).trigger("add-layer", {
+										"id" : portalLayer.id,
+										"groupId" : group.id,
+										"url" : url,
+										"wmsName" : wmsName,
+										"name" : portalLayer.label,
+										"infoLink" : "http://rdc-snsf.org/static/loc/en/html/bluemarble_def.html",
+										"timestamps" : [ "2001-2-23", "2003-5-12" ],
+										"visible" : getValueOrDefault(portalLayer, "active", true)
 									});
-									if (matches.length == 0) {
-										$(document).trigger("trigger", "error",
-												"No portal layer with id '" + items[j] + "', referenced by group " + group.id);
-									} else if (matches.length > 1) {
-										$(document).trigger("trigger", "error", "Two portal layers with id '" + items[j]);
-									} else {
-										var portalLayer = matches[0];
-										$(document).trigger("add-layer", {
-											"id" : portalLayer.id,
-											"groupId" : group.id,
-											"url" : "http://rdc-snsf.org/diss_geoserver/wms",
-											"wmsName" : "common:blue_marble",
-											"name" : portalLayer.label,
-											"infoLink" : "http://rdc-snsf.org/static/loc/en/html/bluemarble_def.html",
-											"timestamps" : [ "2001-2-23", "2003-5-12" ],
-											"visible" : getValueOrDefault(portalLayer, "active", true)
-										});
-									}
+								} else {
+									$(document).trigger("trigger", "error", "One (and only one) wms layer with id '" + id + "' expected");
 								}
+
+							} else {
+								$(document).trigger("trigger", "error", "One (and only one) portal layer with id '" + id + "' expected");
 							}
-						},
-						error : function(jqXHR, textStatus, errorThrown) {
-							$(document).trigger("error", jqXHR.responseText);
 						}
-					});
-
-					$(document).trigger("initial-zoom");
-				});
-
+						
+						$(document).trigger("initial-zoom");
+					}
+				},
+				error : function(jqXHR, textStatus, errorThrown) {
+					$(document).trigger("error", jqXHR.responseText);
+				}
 			});
+
+		});
+
+	});
 });
