6b1ec4493629aa2210d52abd28b56f9b5637ed29
Fernando Gonzalez Cortes <fernando.gonzalez@geomati.co>
2013-10-17 15:03:35 +0200

    error management can return generic HTTP codes using StatusServletException

---
+++ src/main/java/org/fao/unredd/portal/StatusServletExceptionImpl.java
@@ -0,0 +1,23 @@
+package org.fao.unredd.portal;
+
+import javax.servlet.ServletException;
+
+import org.fao.unredd.servlet.StatusServletException;
+
+public class StatusServletExceptionImpl extends ServletException implements
+		StatusServletException {
+	private static final long serialVersionUID = 1L;
+
+	private int status;
+
+	public StatusServletExceptionImpl(int status, String msg) {
+		super(msg);
+		this.status = status;
+	}
+
+	@Override
+	public int getStatus() {
+		return status;
+	}
+
+}
