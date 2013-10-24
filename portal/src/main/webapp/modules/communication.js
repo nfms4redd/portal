276279ee9d1644183f5d93222404de79eac3f701
Fernando Gonzalez <fernando.gonzalez@geomati.co>
2013-10-24 15:33:39 +0200

    Apply the lang parameter only to calls of the application own services

--- portal/src/main/webapp/modules/communication.js
+++ portal/src/main/webapp/modules/communication.js
@@ -6,23 +6,26 @@ define([ "jquery", "message-bus" ], function($, bus) {
 
 	bus.listen("ajax", function(event, ajaxParams) {
 		/*
-		 * The language code is not always available, so we can only get it from
-		 * the URL
+		 * Add "lang" parameter to calls to our server
 		 */
-		var langParameter = "";
-		var langParameterValue = decodeURIComponent((new RegExp('[?|&]lang=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [ , "" ])[1].replace(
-				/\+/g, '%20'))
-				|| null;
-		if (langParameterValue !== null) {
-			langParameter = "lang=" + langParameterValue;
-		}
+		if (ajaxParams.url.indexOf("proxy") !== 0) {
+			/*
+			 * The language code is not always available, so we can only get it
+			 * from the URL
+			 */
+			var langParameter = "";
+			var langParameterValue = decodeURIComponent((new RegExp('[?|&]lang=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [ , "" ])[1].replace(/\+/g, '%20')) || null;
+			if (langParameterValue !== null) {
+				langParameter = "lang=" + langParameterValue;
+			}
 
-		if (ajaxParams.hasOwnProperty("data")) {
-			ajaxParams.data += "&";
-		} else {
-			ajaxParams.data = "";
+			if (ajaxParams.hasOwnProperty("data")) {
+				ajaxParams.data += "&";
+			} else {
+				ajaxParams.data = "";
+			}
+			ajaxParams.data += langParameter;
 		}
-		ajaxParams.data += langParameter;
 
 		ajaxParams.error = function(jqXHR, textStatus, errorThrown) {
 			var message = ajaxParams.errorMsg + ". ";
