c662a98b576c6cdb19bf203233128d1444d54e6a
Fernando Gonzalez <fernando.gonzalez@geomati.co>
2013-10-24 10:29:15 +0200

    Modified exercise

--- doc/workshops/developer_es/client-modules/new-layer.js
+++ doc/workshops/developer_es/client-modules/new-layer.js
@@ -16,28 +16,27 @@ define([ "message-bus", "layout", "jquery", "toolbar" ], function(bus, layout, $
 		var fieldset = $("<fieldset/>").appendTo(form);
 		fieldset.append($("<label/>").attr("for", "layerName").html("Nombre:"));
 		var txtLayerName = $("<input/>").attr("name", "layerName").attr("type", "text");
-		txtLayerName.addClass("text ui-widget-content ui-corner-all");
+		txtLayerName.addClass("add-layer-control text ui-widget-content ui-corner-all");
 		fieldset.append(txtLayerName);
 		fieldset.append($("<label/>").attr("for", "url").html("URL Servidor:"));
 		var txtURL = $("<input/>").attr("name", "url").attr("type", "text");
 		txtURL.val("http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r-t.cgi?");
-		txtURL.addClass("text ui-widget-content ui-corner-all");
+		txtURL.addClass("add-layer-control text ui-widget-content ui-corner-all");
 		fieldset.append(txtURL);
 		fieldset.append($("<label/>").attr("for", "wmsName").html("Nombre WMS:"));
 		var txtWMSName = $("<input/>").attr("name", "wmsName").attr("type", "text");
 		txtWMSName.val("nexrad-n0r-wmst");
-		txtWMSName.addClass("text ui-widget-content ui-corner-all");
+		txtWMSName.addClass("add-layer-control text ui-widget-content ui-corner-all");
 		fieldset.append(txtWMSName);
 		fieldset.append($("<label/>").attr("for", "groupId").html("Grupo:"));
 		var cmbGroups = $("<select/>").attr("name", "groupId");
-		cmbGroups.addClass("ui-widget-content ui-corner-all");
+		cmbGroups.addClass("add-layer-control ui-widget-content ui-corner-all");
 		for (var i = 0; i < groups.length; i++) {
 			var optGroup = $("<option/>").attr("value", groups[i].id).html(groups[i].name);
 			cmbGroups.append(optGroup);
 		}
 		fieldset.append(cmbGroups);
 		div.dialog({
-			autoOpen : true,
 			height : "auto",
 			width : 350,
 			modal : true,
