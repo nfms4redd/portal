8864d0a3229101f88eec45f4d997b73378fe8922
Fernando Gonzalez Cortes <fernando.gonzalez@geomati.co>
2013-10-28 12:44:43 +0100

    updated exercise

--- doc/workshops/developer_es/client-modules/map-scale.js
+++ doc/workshops/developer_es/client-modules/map-scale.js
@@ -1,3 +1,5 @@
-define(["olmap"], function(map){
+define([ "map", "message-bus" ], function(map, bus) {
+	bus.send("css-load", "modules/map-scale.css");
+
 	map.addControl(new OpenLayers.Control.Scale());
 });
