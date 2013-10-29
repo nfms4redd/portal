e6a94ba0e673e940d281540cffcb907a1fec1d10
Fernando Gonzalez Cortes <fernando.gonzalez@geomati.co>
2013-10-29 11:17:01 +0100

    removed unused leaflet path

--- portal/src/main/webapp/modules/main.js
+++ portal/src/main/webapp/modules/main.js
@@ -6,8 +6,7 @@ require.config({
 		"jquery" : "http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min",
 		"jquery-ui" : "http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min",
 		"fancy-box" : "../js/jquery.fancybox.pack",
-		"openlayers" : "../js/OpenLayers/OpenLayers.debug",
-		"leaflet" : "http://cdn.leafletjs.com/leaflet-0.6.4/leaflet"
+		"openlayers" : "../js/OpenLayers/OpenLayers.debug"
 	}
 });
 
