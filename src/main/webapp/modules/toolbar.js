98bb2aec1c49322e0ad168ce10b0b671a478488c
Fernando Gonzalez Cortes <fernando.gonzalez@geomati.co>
2013-11-27 13:50:57 +0100

    Add 'selected' class to selected language button

--- src/main/webapp/modules/toolbar.js
+++ src/main/webapp/modules/toolbar.js
@@ -1,26 +1,17 @@
 define([ "jquery", "message-bus", "layout", "customization", "i18n", "mustache" ], function($, bus, layout, customization, i18n, mustache) {
-	view = {
+	var view = {
 		lang_codes: customization.languages,
-		lang_name: function () { return i18n[this] }
+		lang_name: function () { return i18n[this]; },
+		selectedClass : function() {
+			return this == customization.languageCode ? "selected" : "";
+		}
 	};
 
-	var template = '{{#lang_codes}}<a class="blue_button lang_button" href="?lang={{.}}" id="button_{{.}}">{{lang_name}}</a>{{/lang_codes}}'
-	var output = mustache.render(template, view)
+	var template = '{{#lang_codes}}<a class="blue_button lang_button {{selectedClass}}" href="?lang={{.}}" id="button_{{.}}">{{lang_name}}</a>{{/lang_codes}}';
+	var output = mustache.render(template, view);
 
 	var divToolbar = layout.toolbar;
-	divToolbar.append(output)
-
-	/*
-	for (var i = 0; i < customization.languages.length; i++) {
-		var language = customization.languages[i];
-		var btnLanguage = $("<a href='?lang=" + language + "'/>").attr("id", "button_" + language).html(i18n[language]);
-		btnLanguage.addClass("blue_button lang_button");
-		if (customization.languageCode == language) {
-			btnLanguage.addClass("selected");
-		}
-		divToolbar.append(btnLanguage);
-	}
-	*/
+	divToolbar.append(output);
 
 	return divToolbar;
 });
