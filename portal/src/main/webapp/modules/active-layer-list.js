00262d804a0317d289b8b31b679ebab971d90c0a
Stefano Giaccio <stefano.giaccio@gmail.com>
2013-12-04 16:28:40 +0100

    Using events to activate layers when created
    Also working on the active layer list

--- portal/src/main/webapp/modules/active-layer-list.js
+++ portal/src/main/webapp/modules/active-layer-list.js
@@ -1,37 +1,75 @@
 define([ "jquery", "message-bus", "layout", "jquery-ui" ], function($, bus, layout) {
 	var divActiveLayers = layout.activeLayers;
 
-	var radio1 = $('<input type="radio" id="all_layers" name="layer_list_selector" checked="checked"></input><label for="all_layers">Layers</label>');
-	var radio2 = $('<input type="radio" id="active_layers" name="layer_list_selector"></input><label for="active_layers">Selected layers</label>');
+    divActiveLayers.empty();
+    //bus.listen("layer-visibility", [ layerInfo.id, checked ]);
+    bus.listen("layer-visibility", function(event, layerId, visibility) {
+        //console.log(layerId);
+        //console.log(visibility);
+    });
 
-	divLayerListSelector.append(radio1);
-	divLayerListSelector.append(radio2);
 
-	radio1.click(function() {
-		bus.send("show-layer-list");
-		bus.send("hide-active-layer-list");
-	});
 
-	radio2.click(function () {
-		bus.send("show-active-layer-list");
-		bus.send("hide-layer-list");
-		/*
-		 $("#layers_pane").hide();
-		 $("#active_layers_pane").accordion({
-		 collapsible: false,
-		 autoHeight: false,
-		 animated: false,
-		 create: function (event, ui) {
-		 $('#active_layers_pane .ui-icon-triangle-1-s').hide();
-		 updateActiveLayersPane(mapContexts);
-		 }
-		 });
-		 $("#active_layers_pane").show();
-		 */
-	});
 
-	divLayerListSelector.buttonset();
-	divLayerListSelector.show();
+
+
+
+    if (false) {
+        updateActiveLayersPane = function () {
+            var //div,
+                table, tr, td, td2, layers, inlineLegend, transparencyDiv;
+            // empty the active_layers div (layer on the UI -> context here)
+            $('#active_layers_pane div').empty();
+
+            table = $('<table style="width:90%;margin:auto"></table>');
+            $('#active_layers_pane div').append(table);
+
+            jQuery.each(UNREDD.mapContexts, function (contextName, context) {
+                var contextConf = context.configuration;
+
+                if (contextConf.active) {
+                    // First row: inline legend and context name
+                    tr = $('<tr></tr>');
+
+                    if (contextConf.hasOwnProperty('inlineLegendUrl')) {
+                        td = $('<td style="width:20px"></td>');
+                        inlineLegend = $('<img class="inline-legend" src="' + UNREDD.wmsServers[0] + contextConf.inlineLegendUrl + '">');
+                        td.append(inlineLegend);
+                        tr.append(td);
+                        td2 = $('<td></td>');
+                    } else {
+                        td2 = $('<td colspan="2"></td>');
+                    }
+                    td2.append(contextConf.label);
+                    tr.append(td2);
+                    table.append(tr);
+
+                    // Another row
+                    tr = $('<tr></tr>');
+                    transparencyDiv = $('<div style="margin-top:4px; margin-bottom:12px;" id="' + contextName + '_transparency_slider"></div>');
+                    td = $('<td colspan="2"></td>');
+                    td.append(transparencyDiv);
+                    tr.append(td);
+                    table.append(tr);
+
+                    layers = contextConf.layers;
+
+                    (function (contextLayers) {
+                        $(transparencyDiv).slider({
+                            min: 0,
+                            max: 100,
+                            value: 100,
+                            slide: function (event, ui) {
+                                $.each(contextLayers, function (n, layer) {
+                                    layer.olLayer.setOpacity(ui.value / 100);
+                                });
+                            }
+                        });
+                    }(context.layers));
+                }
+            });
+        };
+    }
 
 	return divActiveLayers;
 });
--- portal/src/main/webapp/modules/layer-list-selector.js
+++ portal/src/main/webapp/modules/layer-list-selector.js
@@ -1,18 +1,18 @@
 define([ "jquery", "message-bus", "layout", "customization", "i18n", "jquery-ui", "layer-list" ], function($, bus, layout, customization, i18n, ui, layerList) {
 	var divLayerListSelector = layout.layerListSelector;
 
-	var radio1 = $('<input type="radio" id="all_layers" name="layer_list_selector" checked="checked"></input><label for="all_layers">' + i18n.layers + '</label>');
-	var radio2 = $('<input type="radio" id="active_layers" name="layer_list_selector"></input><label for="active_layers">' + i18n.selected_layers + '</label>');
+	var allLayersButton = $('<input type="radio" id="all_layers" name="layer_list_selector" checked="checked"></input><label for="all_layers">' + i18n.layers + '</label>');
+	var activeLayersbutton = $('<input type="radio" id="active_layers" name="layer_list_selector"></input><label for="active_layers">' + i18n.selected_layers + '</label>');
 
-	divLayerListSelector.append(radio1);
-	divLayerListSelector.append(radio2);
+	divLayerListSelector.append(allLayersButton);
+	divLayerListSelector.append(activeLayersbutton);
 
-	radio1.click(function() {
+    allLayersButton.click(function() {
 		bus.send("show-layer-list");
 		bus.send("hide-active-layer-list");
 	});
 
-	radio2.click(function () {
+    activeLayersbutton.click(function () {
 		bus.send("show-active-layer-list");
 		bus.send("hide-layer-list");
 		/*
