a2c2f696e0fa79e879e1fae6cbdad8ef55ca79bc
Stefano Giaccio <stefano.giaccio@gmail.com>
2013-12-09 17:15:01 +0100

    Merge branch 'develop' of https://github.com/nfms4redd/nfms into develop

--- src/main/webapp/modules/layers.js
+++ src/main/webapp/modules/layers.js
@@ -20,26 +20,30 @@ define([ "jquery", "message-bus", "customization", "module" ], function($, bus, 
 		}
 	};
 
-	bus.listen("modules-loaded", function() {
-		var layerRoot = module.config();
-		var groups = layerRoot.groups;
-		for (var i = 0; i < groups.length; i++) {
-			var group = groups[i];
-            var groupInfo = {
-                "id" : group.id,
-                "name" : group.label
-            };
-            if (group.hasOwnProperty("infoFile")) {
-                groupInfo.infoLink = "static/loc/" + customization.languageCode + "/html/" + group.infoFile;
-            }
+	var processGroup = function(layerRoot, parentId, group) {
+		var groupInfo = {
+			"id" : group.id,
+			"name" : group.label
+		};
+		if (group.hasOwnProperty("infoFile")) {
+			groupInfo.infoLink = "static/loc/" + customization.languageCode + "/html/" + group.infoFile;
+		}
+		if (parentId !== null) {
+			groupInfo.parentId = parentId;
+		}
 
-            bus.send("add-group", groupInfo);
+		bus.send("add-group", groupInfo);
 
-			var items = group.items;
-			for (var j = 0; j < items.length; j++) {
-				var portalLayer = findById(layerRoot.portalLayers, items[j]);
+		var items = group.items;
+		for (var j = 0; j < items.length; j++) {
+			var item = items[j];
+			if (typeof item === 'object') {
+				processGroup(layerRoot, group.id, item);
+			} else {
+				var portalLayer = findById(layerRoot.portalLayers, item);
 				if (portalLayer != null) {
-					var wmsLayer = findById(layerRoot.wmsLayers, portalLayer.layers[0]);
+					var wmsLayerId = portalLayer.layers[0];
+					var wmsLayer = findById(layerRoot.wmsLayers, wmsLayerId);
 					if (wmsLayer != null) {
 						var url = wmsLayer.baseUrl;
 						var wmsName = wmsLayer.wmsName;
@@ -61,15 +65,22 @@ define([ "jquery", "message-bus", "customization", "module" ], function($, bus, 
 						bus.send("add-layer", layerInfo);
 
                         // Set layer visibility on OpenLayers
-                        bus.send("layer-visibility", [layerInfo.id, layerInfo.visible])
+                        bus.send("layer-visibility", [layerInfo, layerInfo.visible])
 					} else {
-						bus.send("error", "One (and only one) wms layer with id '" + id + "' expected");
+						bus.send("error", "One (and only one) wms layer with id '" + wmsLayerId + "' expected");
 					}
-
 				} else {
-					bus.send("error", "One (and only one) portal layer with id '" + id + "' expected");
+					bus.send("error", "One (and only one) portal layer with id '" + item + "' expected");
 				}
 			}
+		}
+	};
+	
+	bus.listen("modules-loaded", function() {
+		var layerRoot = module.config();
+		var groups = layerRoot.groups;
+		for (var i = 0; i < groups.length; i++) {
+			processGroup(layerRoot, null, groups[i]);
 		}
 
 		bus.send("layers-loaded");
