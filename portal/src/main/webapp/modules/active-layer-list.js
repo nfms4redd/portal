6fbf72ec56e9f0714aa2fcca89a98e98d751432c
Stefano Giaccio <stefano.giaccio@gmail.com>
2013-12-12 12:18:53 +0100

    Fixed a bug where active layers list was not expanded properly to contain all the sliders
    Also ade text unselectable

--- portal/src/main/webapp/modules/active-layer-list.js
+++ portal/src/main/webapp/modules/active-layer-list.js
@@ -1,5 +1,7 @@
 define([ "jquery", "message-bus", "layout", "map", "jquery-ui" ], function($, bus, layout, map) {
-	var divActiveLayers = layout.activeLayers;
+	var divActiveLayersContainer = layout.activeLayers;
+
+    var divActiveLayers = $("<div/>").attr("id", "active_layers");
 
     // Accordion header
 	var h3Title = $("<h3/>").html("Selected layers");
@@ -8,21 +10,24 @@ define([ "jquery", "message-bus", "layout", "map", "jquery-ui" ], function($, bu
     // div that contains all the active layers with sliders
 	var div = $("<div/>");
 	divActiveLayers.append(div);
-	div.empty();
+	//div.empty();
 
     var table = $('<table style="width:100%;margin:auto"></table>');
 	div.append(table);
 
     // create the accordion
-	$("#active_layers_pane").accordion({
+    divActiveLayers.accordion({
 		collapsible: false,
 		autoHeight: false,
 		animated: false,
+        heightStyle: "content",
 		create: function (event, ui) {
 			$('#active_layers_pane .ui-icon-triangle-1-s').hide();
 			//updateActiveLayersPane(mapContexts);
 		}
 	});
+
+    divActiveLayersContainer.append(divActiveLayers);
 
 	bus.listen("layer-visibility", function(event, layerInfo, visibility) {
         var layerId = layerInfo.id;
@@ -51,6 +56,7 @@ define([ "jquery", "message-bus", "layout", "map", "jquery-ui" ], function($, bu
                     bus.send("transparency-slider-changed", [layerInfo, ui.value / 100]);
                 }
             });
+            divActiveLayers.accordion("refresh");
         }
 
         function delLayer(layerId) {
@@ -63,82 +69,23 @@ define([ "jquery", "message-bus", "layout", "map", "jquery-ui" ], function($, bu
     });
 
 	bus.listen("show-active-layer-list", function(event, groupInfo) {
-		divActiveLayers.accordion({
-			collapsible: false,
-			autoHeight: false,
-			animated: false,
-			create: function (event, ui) {
-                // Hide the expand/contract arrow - it doesn't work
-
-                //console.log(divActiveLayers.find('.ui-icon-triangle-1-s'))
-                //divActiveLayers.find('.ui-icon-triangle-1-s').hide();
-				//$('#active_layers_pane .ui-icon-triangle-1-s').hide();
-				//updateActiveLayersPane(mapContexts);
-			}
-		});
-		divActiveLayers.accordion("refresh");
-		divActiveLayers.show();
+//        divActiveLayers.accordion({
+//            collapsible: false,
+//            autoHeight: false,
+//            animated: false,
+//            clearStyle: true,
+//            create: function (event, ui) {
+//                $('#active_layers_pane .ui-icon-triangle-1-s').hide();
+//                //updateActiveLayersPane(mapContexts);
+//            }
+//        });
+        divActiveLayers.accordion("refresh");
+		divActiveLayersContainer.show();
 	});
+
 	bus.listen("hide-active-layer-list", function(event, groupInfo) {
-		divActiveLayers.hide();
+        divActiveLayersContainer.hide();
 	});
-
-	if (false) {
-		updateActiveLayersPane = function () {
-			var //div,
-				table, tr, td, td2, layers, inlineLegend, transparencyDiv;
-			// empty the active_layers div (layer on the UI -> context here)
-			$('#active_layers_pane div').empty();
-
-			table = $('<table style="width:90%;margin:auto"></table>');
-			$('#active_layers_pane div').append(table);
-
-			jQuery.each(UNREDD.mapContexts, function (contextName, context) {
-				var contextConf = context.configuration;
-
-				if (contextConf.active) {
-					// First row: inline legend and context name
-					tr = $('<tr></tr>');
-
-					if (contextConf.hasOwnProperty('inlineLegendUrl')) {
-						td = $('<td style="width:20px"></td>');
-						inlineLegend = $('<img class="inline-legend" src="' + UNREDD.wmsServers[0] + contextConf.inlineLegendUrl + '">');
-						td.append(inlineLegend);
-						tr.append(td);
-						td2 = $('<td></td>');
-					} else {
-						td2 = $('<td colspan="2"></td>');
-					}
-					td2.append(contextConf.label);
-					tr.append(td2);
-					table.append(tr);
-
-					// Another row
-					tr = $('<tr></tr>');
-					transparencyDiv = $('<div style="margin-top:4px; margin-bottom:12px;" id="' + contextName + '_transparency_slider"></div>');
-					td = $('<td colspan="2"></td>');
-					td.append(transparencyDiv);
-					tr.append(td);
-					table.append(tr);
-
-					layers = contextConf.layers;
-
-					(function (contextLayers) {
-						$(transparencyDiv).slider({
-							min: 0,
-							max: 100,
-							value: 100,
-							slide: function (event, ui) {
-								$.each(contextLayers, function (n, layer) {
-									layer.olLayer.setOpacity(ui.value / 100);
-								});
-							}
-						});
-					}(context.layers));
-				}
-			});
-		};
-	}
 
 	return divActiveLayers;
 });
