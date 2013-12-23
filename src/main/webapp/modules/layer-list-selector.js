7f2949142cd782a8d832bf81422c90ae954e0da3
Fernando Gonzalez <fernando.gonzalez@geomati.co>
2013-12-23 09:40:15 +0100

    Fixes #90. Button id collision with panel id

--- src/main/webapp/modules/layer-list-selector.js
+++ src/main/webapp/modules/layer-list-selector.js
@@ -1,8 +1,8 @@
 define([ "jquery", "message-bus", "layout", "customization", "i18n", "jquery-ui", "layer-list" ], function($, bus, layout, customization, i18n, ui, layerList) {
 	var divLayerListSelector = layout.layerListSelector;
 
-	var allLayersButton = $('<input type="radio" id="all_layers" name="layer_list_selector" checked="checked"></input><label for="all_layers">' + i18n.layers + '</label>');
-	var activeLayersbutton = $('<input type="radio" id="active_layers" name="layer_list_selector"></input><label for="active_layers">' + i18n.selected_layers + '</label>');
+	var allLayersButton = $('<input type="radio" id="show_all_layers" name="layer_list_selector" checked="checked"></input><label for="show_all_layers">' + i18n.layers + '</label>');
+	var activeLayersbutton = $('<input type="radio" id="show_active_layers" name="layer_list_selector"></input><label for="show_active_layers">' + i18n.selected_layers + '</label>');
 
 	divLayerListSelector.append(allLayersButton);
 	divLayerListSelector.append(activeLayersbutton);
