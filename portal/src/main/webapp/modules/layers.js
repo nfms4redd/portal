b8484f866a33b9965bc80bc6a001616fdec593e7
Stefano Giaccio <stefano.giaccio@gmail.com>
2014-01-14 17:04:56 +0100

    Working on issue 84 (multiple wms layer per portal layer)

--- portal/src/main/webapp/modules/layers.js
+++ portal/src/main/webapp/modules/layers.js
@@ -9,22 +9,37 @@ define([ "jquery", "message-bus", "customization", "module" ], function($, bus, 
 		}
 	};
 
-	var findById = function(array, id) {
-		var matches = $.grep(array, function(l) {
-			return l.id == id;
-		});
-		if (matches.length == 1) {
-			return matches[0];
-		} else {
-			return null;
-		}
-	};
+    var findById = function(array, id) {
+        return $.grep(array, function(l) {
+            return l.id === id;
+        });
+    };
 
-	var processGroup = function(layerRoot, parentId, group) {
+    bus.listen("add-portal-layer", function(event, portalLayer) {
+        var wmsLayers = portalLayer.wmsLayers;
+        for (var i = 0; i < wmsLayers.length; i++) {
+            bus.send("add-layer", wmsLayers[i]);
+        }
+    })
+
+    bus.listen("portal-layer-visibility", function(event, portalLayer) {
+        var wmsLayers = portalLayer.wmsLayers;
+
+        for (var i = 0; i < wmsLayers.length; i++) {
+            bus.send("layer-visibility", [wmsLayers[i], getValueOrDefault(portalLayer, "active", false)])
+            if (getValueOrDefault(portalLayer, "active", false)) console.log(wmsLayers[i]);
+        }
+    })
+
+    var processGroup = function(layerRoot, parentId, group) {
+        var items, item, portalLayers, portalLayer, wmsLayerIds,
+            wmsLayers, wmsLayer, i, j;
+
 		var groupInfo = {
 			"id" : group.id,
 			"name" : group.label
 		};
+
 		if (group.hasOwnProperty("infoFile")) {
 			groupInfo.infoLink = "static/loc/" + customization.languageCode + "/html/" + group.infoFile;
 		}
@@ -34,65 +49,60 @@ define([ "jquery", "message-bus", "customization", "module" ], function($, bus, 
 
 		bus.send("add-group", groupInfo);
 
-		var items = group.items;
-		for (var j = 0; j < items.length; j++) {
-			var item = items[j];
+		items = group.items;
+
+		for (i = 0; i < items.length; i++) {
+			item = items[i];
 			if (typeof item === 'object') {
 				processGroup(layerRoot, group.id, item);
 			} else {
-				var portalLayer = findById(layerRoot.portalLayers, item);
-				if (portalLayer != null) {
-					var wmsLayerId = portalLayer.layers[0];
-					var wmsLayer = findById(layerRoot.wmsLayers, wmsLayerId);
-					if (wmsLayer != null) {
-						var url = wmsLayer.baseUrl;
-						var wmsName = wmsLayer.wmsName;
-						var layerInfo = {
-							"id" : portalLayer.id,
-							"groupId" : group.id,
-							"url" : url,
-							"wmsName" : wmsName,
-							"name" : portalLayer.label,
-							"queryable" : wmsLayer.queryable,
-							"visible" : getValueOrDefault(portalLayer, "active", false)
-						};
-						if (portalLayer.hasOwnProperty("infoFile")) {
-							layerInfo.infoLink = "static/loc/" + customization.languageCode + "/html/" + portalLayer.infoFile;
-						}
-						if (wmsLayer.hasOwnProperty("wmsTime")) {
-							layerInfo.timestamps = wmsLayer.wmsTime.split(",");
-						}
-						if (wmsLayer.hasOwnProperty("legend")) {
-							layerInfo.legendURL = "static/loc/" + customization.languageCode + "/images/" + wmsLayer.legend;
-						}
-						if (wmsLayer.hasOwnProperty("sourceLink")) {
-							layerInfo.sourceLink = wmsLayer.sourceLink;
-						}
-						if (wmsLayer.hasOwnProperty("sourceLabel")) {
-							layerInfo.sourceLabel = wmsLayer.sourceLabel;
-						}
+			    portalLayers = findById(layerRoot.portalLayers, item);
+                if (portalLayers.length !== 1) {
+                    bus.send("error", "One (and only one) portal layer with id '" + item + "' expected");
+                    continue;
+                }
 
-						bus.send("add-layer", layerInfo);
+                portalLayer = portalLayers[0];
+                wmsLayerIds = portalLayer.layers;
 
-                        // Set layer visibility on OpenLayers
-                        bus.send("layer-visibility", [layerInfo, layerInfo.visible])
-					} else {
-						bus.send("error", "One (and only one) wms layer with id '" + wmsLayerId + "' expected");
-					}
-				} else {
-					bus.send("error", "One (and only one) portal layer with id '" + item + "' expected");
-				}
+                var layerInfoArray = [];
+
+                // Iterate over wms layers
+                for (j = 0; j < wmsLayerIds.length; j++) {
+                    wmsLayers = findById(layerRoot.wmsLayers, wmsLayerIds[j]);
+                    if (wmsLayers.length === 0) {
+                        bus.send("error", "At least one layer with id '" + wmsLayerIds[j] + "' expected");
+                        continue;
+                    }
+                    wmsLayer = wmsLayers[0];
+                    if (wmsLayer.hasOwnProperty("wmsTime")) {
+                        wmsLayer.timestamps = wmsLayer.wmsTime.split(",")
+                    }
+
+                    //bus.send("add-layer", layerInfo);
+                    layerInfoArray.push(wmsLayer);
+                }
+
+                portalLayer.groupId = group.id
+                portalLayer.wmsLayers = layerInfoArray;
+
+                bus.send("add-portal-layer", portalLayer);
+                bus.send("portal-layer-visibility", portalLayer);
 			}
 		}
 	};
 
+
 	bus.listen("modules-loaded", function() {
+        var i;
 		var layerRoot = module.config();
 		var groups = layerRoot.groups;
-		for (var i = 0; i < groups.length; i++) {
+
+		for (i = 0; i < groups.length; i++) {
 			processGroup(layerRoot, null, groups[i]);
 		}
 
 		bus.send("layers-loaded");
 	});
+
 });
