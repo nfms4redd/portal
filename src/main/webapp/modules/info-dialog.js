7defcf7fb41f4a2410908ad46ce1630b2bf14843
Stefano Giaccio <stefano.giaccio@gmail.com>
2014-01-21 13:49:57 +0100

    Features are highlighted when hovering the info dialog - closes #91

--- src/main/webapp/modules/info-dialog.js
+++ src/main/webapp/modules/info-dialog.js
@@ -1,15 +1,17 @@
-define([ "jquery", "message-bus" ], function($, bus) {
+define([ "jquery", "message-bus", "map" ], function($, bus, map) {
 
 	bus.listen("info-features", function(event, features, x, y) {
+		var i, infoPopup, dialogX, dialogY, epsg4326, epsg900913;
+
 		// re-project to Google projection
-		var epsg4326 = new OpenLayers.Projection("EPSG:4326");
-		var epsg900913 = new OpenLayers.Projection("EPSG:900913");
-		for (var i = 0; i < features.length; i++) {
+		epsg4326 = new OpenLayers.Projection("EPSG:4326"),
+		epsg900913 = new OpenLayers.Projection("EPSG:900913");
+		for (i = 0; i < features.length; i++) {
 			features[i].geometry.transform(epsg4326, epsg900913);
 		}
 
-		var infoPopup = $("#info_popup");
-		if (infoPopup.length == 0) {
+		infoPopup = $("#info_popup");
+		if (infoPopup.length === 0) {
 			infoPopup = $("<div/>").attr("id", "info_popup");
 		} else {
 			infoPopup.empty();
@@ -19,11 +21,15 @@ define([ "jquery", "message-bus" ], function($, bus) {
 			width : 300,
 			resizable : false,
 			close : function(event, ui) {
+				bus.send("clear-highlighted-features");
+				map.getLayer("Highlighted Features").destroyFeatures();
+
 				// destroy all features
-				$.each(features, function(featureIndex, feature) {
-					feature.destroy();
-				});
+//				$.each(features, function(featureIndex, feature) {
+//					feature.destroy();
+//				});
 			},
+
 			autoOpen : false
 		});
 
@@ -31,43 +37,46 @@ define([ "jquery", "message-bus" ], function($, bus) {
 		// standard one
 
 		$.each(features, function(layerId, feature) {
-			var qualifiedLayerId = feature.gml.featureNSPrefix + ":" + feature.gml.featureType;
+			var qualifiedLayerId, table, tr1, td1, tr2, td2, td3, info;
 
-			var table = $("<table/>");
-			var tr1 = $("<tr/>");
-			var td1 = $('<td colspan="2" class="area_name" />');
+			qualifiedLayerId = feature.gml.featureNSPrefix + ":" + feature.gml.featureType;
+
+			table = $("<table/>");
+			tr1 = $("<tr/>");
+			td1 = $('<td colspan="2" class="area_name" />');
 			tr1.append(td1);
 			table.append(tr1);
-			table.mouseover(function() {
-				bus.send("highlight-feature", [ feature ]);
-			});
-			table.mouseout(function() {
-				bus.send("clear-highlighted-features");
-			});
 
-			var info = $("<table/>");
+			table.mouseenter(function() { bus.send("highlight-feature", feature); });
+			table.mouseleave(function()  { bus.send("clear-highlighted-features"); });
+
+			info = $("<table/>");
 			$.each(feature.attributes, function(index, attribute) {
 				var tdIndex = $("<td/>").html(index);
 				var tdAttribute = $("<td/>").html(":&nbsp;&nbsp;&nbsp;" + attribute);
 				var trAttribute = $("<tr/>").append(tdIndex).append(tdAttribute);
+
 				info.append(trAttribute);
 			});
 			td1.append(info);
 
 			tr2 = $("<tr/>");
-			var td2 = $("<td class=\"td_left\" id=\"indicator_buttons_container_" + layerId + "\"/>");
+			td2 = $("<td class=\"td_left\" id=\"indicator_buttons_container_" + layerId + "\"/>");
 			tr2.append(td2);
 			table.append(tr2);
 
 			td2.append("<img src=\"styles/images/ajax-loader.gif\" alt=\"wait\"/>");
+
 			bus.send("ajax", {
 				url : 'indicators?layerId=' + qualifiedLayerId,
 				success : function(indicators, textStatus, jqXHR) {
-					for (i = 0; i < indicators.length; i++) {
-						id = "stats_link_" + layerId + "_" + indicators[i].id;
+					var n, id;
+
+					for (n = 0; n < indicators.length; n++) {
+						id = "stats_link_" + layerId + "_" + indicators[n].id;
 						$("#indicator_buttons_container_" + layerId).append(
-								"<a style=\"color:white\" class=\"feature_link fancybox.iframe\" id=\"" + id + "\" href=\"indicator?objectId=" + feature.attributes[indicators[i].fieldId]
-										+ "&layerId=" + qualifiedLayerId + "&indicatorId=" + indicators[i].id + "\">" + indicators[i].name + "</a>");
+								"<a style=\"color:white\" class=\"feature_link fancybox.iframe\" id=\"" + id + "\" href=\"indicator?objectId=" + feature.attributes[indicators[n].fieldId]
+										+ "&layerId=" + qualifiedLayerId + "&indicatorId=" + indicators[n].id + "\">" + indicators[n].name + "</a>");
 						$('#' + id).fancybox({
 							maxWidth : 840,
 							maxHeight : 600,
@@ -100,13 +109,12 @@ define([ "jquery", "message-bus" ], function($, bus) {
 
 		// If no features selected then close the dialog
 		if (features.length === 0) {
-			console.log("closing the dialog");
 			infoPopup.dialog('close');
 		} else {
 			// Don't reposition the dialog if already open
 			if (!infoPopup.dialog('isOpen')) {
-				var dialogX = x - 100;
-				var dialogY = y - 200;
+				dialogX = x + 100;
+				dialogY = y - 200;
 				infoPopup.dialog('option', 'position', [ dialogX, dialogY ]);
 
 				// Finally open the dialog
@@ -115,4 +123,18 @@ define([ "jquery", "message-bus" ], function($, bus) {
 			}
 		}
 	});
+
+	bus.listen("highlight-feature", function(event, feature) {
+		var highlightLayer = map.getLayer("Highlighted Features");
+		highlightLayer.removeAllFeatures();
+		highlightLayer.addFeatures(feature);
+		highlightLayer.redraw();
+	});
+
+	bus.listen("clear-highlighted-features", function(event, feature) {
+		var highlightLayer = map.getLayer("Highlighted Features");
+		highlightLayer.removeAllFeatures();
+		highlightLayer.redraw();
+	});
+
 });
