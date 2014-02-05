7a3ed059833001aef47dec6570b321d28365ec37
Stefano Giaccio <stefano.giaccio@gmail.com>
2014-02-05 11:48:46 +0100

    Solved a compatibility issue with IE8 in timeSlider.js
    Object.keys is not supported in IE8
    Slider label now changes while sliding (not on value change)

--- src/main/webapp/modules/time-slider.js
+++ src/main/webapp/modules/time-slider.js
@@ -1,8 +1,6 @@
 define([ "jquery", "message-bus", "layout", "jquery-ui", "i18n" ], function($, bus, layout, ui, i18n) {
 
-	var divTimeSliderLabel, div, divTimeSlider;
 	var timestampSet = {};
-	var timestamps;
 
 	var getLocalizedDate = function(date) {
 		var defaultMonths = [ "Jan.", "Feb.", "Mar.", "Apr.", "May", "June", "July", "Aug.", "Sep.", "Oct.", "Nov.", "Dec." ];
@@ -29,31 +27,37 @@ define([ "jquery", "message-bus", "layout", "jquery-ui", "i18n" ], function($, b
 	});
 
 	bus.listen("layers-loaded", function() {
-		timestamps = Object.keys(timestampSet).sort();
+		var timestamps, div, divTimeSlider, divTimeSliderLabel, lastTimestampIndex;
+
+		timestamps = $.map(timestampSet, function(value, key) {
+			return key;
+		}).sort();
+		lastTimestampIndex = timestamps.length - 1;
+
 		if (timestamps.length > 0) {
 			div = layout.timeSlider;
-			divTimeSlider = $("<div/>").attr("id", "time_slider");
+
+			divTimeSlider = $('<div id="time_slider"/>');
 			div.append(divTimeSlider);
-			divTimeSlider.slider({
-				change : function(event, ui) {
-					var d = new Date();
 
-					d.setISO8601(timestamps[ui.value]);
-					divTimeSliderLabel.text(getLocalizedDate(timestamps[ui.value]));
-					bus.send("time-slider.selection", d);
-				}
-			});
-			divTimeSlider.slider("option", "min", 0);
-//			divTimeSlider.slider("option", "max", 0);
-
-			divTimeSliderLabel = $("<div/>").attr("id", "time_slider_label");
+			divTimeSliderLabel = $('<div id="time_slider_label"/>');
 			div.append(divTimeSliderLabel);
 
+			divTimeSlider.slider({
+				change: function(event, ui) {
+					var d = (new Date()).setISO8601(timestamps[ui.value]);
+					bus.send("time-slider.selection", d);
+				},
+				slide: function(event, ui) {
+					divTimeSliderLabel.text(getLocalizedDate(timestamps[ui.value]));
+				},
+				max: lastTimestampIndex,
+				value: lastTimestampIndex
+			});
 
-			divTimeSlider.slider("option", "max", timestamps.length - 1);
-			divTimeSlider.slider("value", timestamps.length - 1);
+			divTimeSliderLabel.text(getLocalizedDate(timestamps[lastTimestampIndex]));
 
-			layout.timeSlider.show();
+			div.show();
 		}
 	});
 });
