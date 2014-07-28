a173a112dd1036265e4b3a58e9dd62cdc75fe0e6
Fernando González <fernando.gonzalez@geomati.co>
2014-07-28 12:45:11 +0200

    Show several layers in the dialog

--- src/main/webapp/modules/info-dialog.js
+++ src/main/webapp/modules/info-dialog.js
@@ -1,12 +1,25 @@
-define([ "jquery", "message-bus", "map", "i18n" ], function($, bus, map, i18n) {
+define([ "jquery", "message-bus", "map", "i18n", "customization" ], function($, bus, map, i18n, customization) {
+
+	var wmsNamePortalLayerName = {};
+
+	bus.listen("add-layer", function(event, layerInfo) {
+		var portalLayerName = layerInfo["label"];
+		$.each(layerInfo.wmsLayers, function(i, wmsLayer) {
+			var wmsName = wmsLayer["wmsName"];
+			wmsNamePortalLayerName[wmsName] = portalLayerName;
+		});
+	});
 
 	bus.listen("info-features", function(event, features, x, y) {
 		var i, infoPopup, dialogX, dialogY, epsg4326, epsg900913;
 
 		// re-project to Google projection
-		epsg4326 = new OpenLayers.Projection("EPSG:4326"),
+		epsg4326 = new OpenLayers.Projection("EPSG:4326");
 		epsg900913 = new OpenLayers.Projection("EPSG:900913");
 		for (i = 0; i < features.length; i++) {
+			if (customization["highlight-bounds"] == "true") {
+				features[i].geometry = features[i].geometry.getBounds().toGeometry();
+			}
 			features[i].geometry.transform(epsg4326, epsg900913);
 		}
 
@@ -17,17 +30,14 @@ define([ "jquery", "message-bus", "map", "i18n" ], function($, bus, map, i18n) {
 			infoPopup.empty();
 		}
 		infoPopup.dialog({
+			title : i18n["info_dialog_title"],
 			closeOnEscape : true,
-			width : 300,
-			resizable : false,
+			width : 700,
+			height : 200,
+			resizable : true,
 			close : function(event, ui) {
 				bus.send("clear-highlighted-features");
 				map.getLayer("Highlighted Features").destroyFeatures();
-
-				// destroy all features
-//				$.each(features, function(featureIndex, feature) {
-//					feature.destroy();
-//				});
 			},
 
 			autoOpen : false
@@ -36,75 +46,58 @@ define([ "jquery", "message-bus", "map", "i18n" ], function($, bus, map, i18n) {
 		// TODO check if there is a custom pop up instead of showing the
 		// standard one
 
+		var layerNameFeatures = {};
 		$.each(features, function(layerId, feature) {
-			var qualifiedLayerId, table, tr1, td1, tr2, td2, td3, info;
-
 			qualifiedLayerId = feature.gml.featureNSPrefix + ":" + feature.gml.featureType;
 
-			table = $("<table/>");
-			tr1 = $("<tr/>");
-			td1 = $('<td colspan="2" class="area_name" />');
-			tr1.append(td1);
-			table.append(tr1);
+			if (!layerNameFeatures.hasOwnProperty(qualifiedLayerId)) {
+				layerNameFeatures[qualifiedLayerId] = [ feature ];
+			} else {
+				layerNameFeatures[qualifiedLayerId].push(feature);
+			}
+		});
+		var divResults = $("<div/>").attr("id", "result_area").appendTo(infoPopup);
+		$.each(layerNameFeatures, function(layerId, layerFeatures) {
+			var layerName = wmsNamePortalLayerName[layerId];
+			$("<div/>").addClass("layer_title info_center").html(layerName).appendTo(divResults);
+			var divTable = $("<div/>").addClass("layer_results info_center").appendTo(divResults);
+			var tblData = $("<table/>").appendTo(divTable);
+			var tr = $("<tr/>").appendTo(tblData);
 
-			table.mouseenter(function() { bus.send("highlight-feature", feature); });
-			table.mouseleave(function()  { bus.send("clear-highlighted-features"); });
+			$("<th/>").addClass("command").html("").appendTo(tr);
+			$("<th/>").addClass("command").html("").appendTo(tr);
+			for (attribute in layerFeatures[0].attributes) {
+				$("<th/>").addClass("data").html(attribute).appendTo(tr);
+			}
+			$.each(layerFeatures, function(index, feature) {
+				var tr = $("<tr/>").appendTo(tblData);
 
-			info = $("<table/>");
-			$.each(feature.attributes, function(index, attribute) {
-				var tdIndex = $("<td/>").html(index);
-				var tdAttribute = $("<td/>").html(":&nbsp;&nbsp;&nbsp;" + attribute);
-				var trAttribute = $("<tr/>").append(tdIndex).append(tdAttribute);
+				var imgZoomToArea = $("<img/>").attr("src", "modules/images/zoom-to-object.png");
+				imgZoomToArea.css("cursor", "pointer");
+				$("<td/>").addClass("command").append(imgZoomToArea).appendTo(tr).click(function() {
+					bus.send("zoom-to", feature.geometry.getBounds().scale(1.2));
+				});
 
-				info.append(trAttribute);
-			});
-			td1.append(info);
+				var imgIndicators = $("<img/>").attr("src", "modules/images/object-indicators.png");
+				imgIndicators.css("cursor", "pointer");
+				imgIndicators.click(function() {
+				});
+				$("<td/>").addClass("command").append(imgIndicators).appendTo(tr);
 
-			tr2 = $("<tr/>");
-			td2 = $("<td class=\"td_left\" id=\"indicator_buttons_container_" + layerId + "\"/>");
-			tr2.append(td2);
-			table.append(tr2);
-
-			td2.append("<img src=\"styles/images/ajax-loader.gif\" alt=\"wait\"/>");
-
-			bus.send("ajax", {
-				url : 'indicators?layerId=' + qualifiedLayerId,
-				success : function(indicators, textStatus, jqXHR) {
-					var n, id;
-
-					for (n = 0; n < indicators.length; n++) {
-						id = "stats_link_" + layerId + "_" + indicators[n].id;
-						$("#indicator_buttons_container_" + layerId).append(
-								"<a style=\"color:white\" class=\"feature_link fancybox.iframe\" id=\"" + id + "\" href=\"indicator?objectId=" + feature.attributes[indicators[n].fieldId]
-										+ "&layerId=" + qualifiedLayerId + "&indicatorId=" + indicators[n].id + "\">" + indicators[n].name + "</a>");
-						$('#' + id).fancybox({
-							maxWidth : 840,
-							maxHeight : 600,
-							fitToView : false,
-							width : 840,
-							height : 590,
-							autoSize : false,
-							closeClick : false,
-							openEffect : 'none',
-							closeEffect : 'fade'
-						});
-					}
-				},
-				errorMsg : "Could not obtain the indicator",
-				complete : function() {
-					$("#indicator_buttons_container_" + layerId).children("img").remove();
+				var attributes = feature.attributes;
+				for (attribute in attributes) {
+					$("<td/>").addClass("data").html(attributes[attribute]).appendTo(tr);
 				}
+
+				tr.mouseenter(function() {
+					bus.send("highlight-feature", feature);
+				});
+				tr.mouseleave(function() {
+					bus.send("clear-highlighted-features");
+				});
+
 			});
 
-			// TODO: localize statistics and zoom to area buttons
-			td3 = $("<td class=\"td_right\"/>");
-			td3.append("<a style=\"color:white\" class=\"feature_link\" href=\"#\" id=\"zoom_to_feature_" + layerId + "\">"+ i18n["zoom_to_area"] + "</a>");
-			tr2.append(td3);
-			infoPopup.append(table);
-
-			$("#zoom_to_feature_" + layerId).click(function() {
-				bus.send("zoom-to", feature.geometry.getBounds().scale(1.2));
-			});
 		});
 
 		// If no features selected then close the dialog
