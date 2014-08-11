98b04ed0ea5762f3c4509c289801a727970dd57a
Fernando González <fernando.gonzalez@geomati.co>
2014-08-11 15:07:25 +0200

    Scan also WEB-INF/classes to discover requirejs modules and style sheets

--- core/src/main/java/org/fao/unredd/AppContextListener.java
+++ core/src/main/java/org/fao/unredd/AppContextListener.java
@@ -2,8 +2,10 @@ package org.fao.unredd;
 
 import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.zip.ZipEntry;
@@ -37,6 +39,48 @@ public class AppContextListener implements ServletContextListener {
 
 		ArrayList<String> js = new ArrayList<String>();
 		ArrayList<String> css = new ArrayList<String>();
+		scanJars(servletContext, js, css);
+		scanClasses(servletContext, js, css);
+		servletContext.setAttribute("js-paths", js);
+		servletContext.setAttribute("css-paths", css);
+
+	}
+
+	private void scanClasses(ServletContext servletContext,
+			ArrayList<String> js, ArrayList<String> css) {
+		File rootFolder = new File(
+				servletContext.getRealPath("WEB-INF/classes/nfms"));
+		File modulesFolder = new File(rootFolder, "modules");
+		File stylesFolder = new File(rootFolder, "styles");
+		scanChildren(rootFolder.toURI(), modulesFolder, ".js", js);
+		scanChildren(rootFolder.toURI(), modulesFolder, ".css", css);
+		scanChildren(rootFolder.toURI(), stylesFolder, ".css", css);
+	}
+
+	private void scanChildren(URI referenceURI, File currentFolder,
+			final String extension, ArrayList<String> collection) {
+		File[] files = currentFolder.listFiles(new FileFilter() {
+
+			@Override
+			public boolean accept(File file) {
+				return file.getName().endsWith(extension) || file.isDirectory();
+			}
+		});
+
+		if (files != null) {
+			for (File file : files) {
+				if (file.isDirectory()) {
+					scanChildren(referenceURI, file, extension, collection);
+				} else {
+					collection.add(referenceURI.relativize(file.toURI())
+							.getPath());
+				}
+			}
+		}
+	}
+
+	private void scanJars(ServletContext servletContext, ArrayList<String> js,
+			ArrayList<String> css) {
 		Set<String> libJars = servletContext.getResourcePaths("/WEB-INF/lib");
 		for (Object jar : libJars) {
 			InputStream jarStream = servletContext.getResourceAsStream(jar
@@ -57,7 +101,9 @@ public class AppContextListener implements ServletContextListener {
 						}
 					} else if (entryPath.startsWith("nfms/styles")
 							&& entryPath.endsWith(".css")) {
-						String relativePath = new File("nfms/styles").toURI().relativize(new File(entryPath).toURI()).getPath();
+						String relativePath = new File("nfms/styles").toURI()
+								.relativize(new File(entryPath).toURI())
+								.getPath();
 						css.add("styles/" + relativePath);
 					}
 				}
@@ -69,8 +115,7 @@ public class AppContextListener implements ServletContextListener {
 				} catch (IOException e) {
 				}
 			}
-			servletContext.setAttribute("js-paths", js);
-			servletContext.setAttribute("css-paths", css);
+
 		}
 	}
 
