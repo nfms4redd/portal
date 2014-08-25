bb038b270554a49129763962c82843dcce59ffa6
Fernando González <fernando.gonzalez@geomati.co>
2014-08-25 09:57:21 +0200

    center checks the url parameters for configuration

--- src/main/webapp/WEB-INF/default_config/modules/center.js
+++ src/main/webapp/WEB-INF/default_config/modules/center.js
@@ -1,10 +1,21 @@
-define([ "map", "message-bus", "customization" ], function(map, bus, customization) {
+define([ "map", "message-bus", "customization", "url-parameters" ], function(map, bus, customization, urlParams) {
 
 	var initialZoom = function() {
 		var epsg4326 = new OpenLayers.Projection("EPSG:4326");
-		var center = new OpenLayers.LonLat(customization["map.centerLonLat"]);
+
+		var center;
+		var urlCenter = urlParams.get("map.centerLonLat");
+		if (urlCenter != null) {
+			center = new OpenLayers.LonLat(urlCenter.split(","));
+		} else {
+			center = new OpenLayers.LonLat(customization["map.centerLonLat"]);
+		}
 		center.transform(epsg4326, map.projection);
-		map.setCenter(center, customization["map.initialZoomLevel"]);
+		var zoomLevel = urlParams.get("map.initialZoomLevel");
+		if (zoomLevel == null) {
+			zoomLevel = customization["map.initialZoomLevel"];
+		}
+		map.setCenter(center, zoomLevel);
 	};
 
 	bus.listen("layers-loaded", initialZoom);
