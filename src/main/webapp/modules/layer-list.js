46fa91d96692576590fff23f05f1aa4d13612daf
manureta <manuel.retamozo@gmail.com>
2014-09-19 11:05:59 -0300

    Sinc layer_time_sliders with time_slider global and display year in layer list. Add message 'layer-time-slider.selection' with parmas layerid and date

--- src/main/webapp/modules/layer-list.js
+++ src/main/webapp/modules/layer-list.js
@@ -1,220 +1,311 @@
-define([ "jquery", "message-bus", "layout", "jquery-ui", "fancy-box" ], function($, bus, layout) {
+define(
+		[ "jquery", "message-bus", "layout", "jquery-ui", "fancy-box" ],
+		function($, bus, layout) {
 
-	var temporalLayers = new Array();
+			var temporalLayers = new Array();
 
-	var divLayers = null;
+			var divLayers = null;
 
-	var divLayersContainer = layout.layers;
+			var divLayersContainer = layout.layers;
 
-	divLayers = $("<div/>").attr("id", "all_layers");
-	divLayers.addClass("ui-accordion-icons");
-	divLayers.accordion({
-		"animate": false,
-		"heightStyle": "content",
-		/*
-		 * Collapse all content since otherwise the accordion sets the 'display'
-		 * to 'block' instead than to 'table'
-		 */
-		"collapsible": true,
-		"active": false
-	});
-	divLayersContainer.append(divLayers);
+			divLayers = $("<div/>").attr("id", "all_layers");
+			divLayers.addClass("ui-accordion-icons");
+			divLayers.accordion({
+				"animate" : false,
+				"heightStyle" : "content",
+				/*
+				 * Collapse all content since otherwise the accordion sets the
+				 * 'display' to 'block' instead than to 'table'
+				 */
+				"collapsible" : true,
+				"active" : false
+			});
+			divLayersContainer.append(divLayers);
 
-	bus.listen("add-group", function(event, groupInfo) {
-		var divTitle, tblLayerGroup, parentId, tblParentLayerGroup, divContent;
+			bus
+					.listen(
+							"add-group",
+							function(event, groupInfo) {
+								var divTitle, tblLayerGroup, parentId, tblParentLayerGroup, divContent;
 
-		divTitle = $("<div/>").html(groupInfo.name).disableSelection();
+								divTitle = $("<div/>").html(groupInfo.name)
+										.disableSelection();
 
-		if (groupInfo.hasOwnProperty("infoLink")) {
-			infoButton = $('<a style="position:absolute;top:3px;right:4px;width:16px;height:16px;padding:0;" class="layer_info_button" href="' + groupInfo.infoLink + '"></a>');
+								if (groupInfo.hasOwnProperty("infoLink")) {
+									infoButton = $('<a style="position:absolute;top:3px;right:4px;width:16px;height:16px;padding:0;" class="layer_info_button" href="'
+											+ groupInfo.infoLink + '"></a>');
 
-			// prevent accordion item from expanding when clicking on the info button
-			infoButton.click(function (event) { event.stopPropagation() });
+									// prevent accordion item from expanding
+									// when clicking on the info button
+									infoButton.click(function(event) {
+										event.stopPropagation()
+									});
 
-			infoButton.fancybox({
-				'autoScale' : false,
-				'openEffect' : 'elastic',
-				'closeEffect' : 'elastic',
-				'type': 'ajax',
-				'overlayOpacity': 0.5
+									infoButton.fancybox({
+										'autoScale' : false,
+										'openEffect' : 'elastic',
+										'closeEffect' : 'elastic',
+										'type' : 'ajax',
+										'overlayOpacity' : 0.5
+									});
+
+									divTitle.append(infoButton);
+								}
+
+								tblLayerGroup = $("<table/>");
+								tblLayerGroup.attr("id", "group-content-table-"
+										+ groupInfo.id);
+
+								if (groupInfo.hasOwnProperty("parentId")) {
+									parentId = groupInfo.parentId;
+									tblParentLayerGroup = $("#group-content-table-"
+											+ parentId);
+									if (tblParentLayerGroup.length == 0) {
+										bus
+												.send(
+														"error",
+														"Group "
+																+ groupInfo.name
+																+ " references nonexistent group: "
+																+ parentId);
+									}
+									tblParentLayerGroup.append(divTitle)
+											.append(tblLayerGroup);
+								} else {
+									divLayers.append(divTitle);
+									divContent = $("<div/>").css("padding",
+											"10px 2px 10px 2px");
+									divContent.append(tblLayerGroup);
+									divLayers.append(divContent).accordion(
+											"refresh");
+								}
+							});
+
+			bus
+					.listen(
+							"add-layer",
+							function(event, portalLayer) {
+								var tblLayerGroup, trLayer, tdLegend, tdVisibility, divCheckbox, tdName, tdInfo, aLink, inlineLegend;
+
+								tblLayerGroup = $("#group-content-table-"
+										+ portalLayer.groupId);
+								if (tblLayerGroup.length == 0) {
+									bus.send("error", "Layer "
+											+ portalLayer.label
+											+ " references nonexistent group: "
+											+ portalLayer.groupId);
+								} else {
+									trLayer = $("<tr/>").attr("id",
+											"layer-row-" + portalLayer.id)
+											.addClass("layer_row");
+
+									tdLegend = $("<td/>").addClass(
+											"layer_legend");
+
+									if (portalLayer
+											.hasOwnProperty("inlineLegendUrl")) {
+										// context has an inline legend
+										// tdLegend = $('<td
+										// style="width:20px">');
+										// inlineLegend = $('<img
+										// class="inline-legend" src="' +
+										// UNREDD.wmsServers[0] +
+										// contextConf.inlineLegendUrl + '">');
+										inlineLegend = $('<img class="inline-legend" src="'
+												+ portalLayer.inlineLegendUrl
+												+ '">');
+										tdLegend.append(inlineLegend);
+									} else {
+										var wmsLayersWithLegend = portalLayer.wmsLayers
+												.filter(function(layer) {
+													return layer
+															.hasOwnProperty("legend");
+												});
+										var wmsLayerWithLegend = wmsLayersWithLegend[0];
+
+										if (wmsLayerWithLegend) {
+											inlineLegend = $("<td/>");
+											inlineLegend
+													.addClass("inline-legend-button");
+
+											if (portalLayer.active) {
+												inlineLegend
+														.addClass("visible");
+											}
+
+											bus
+													.listen(
+															"layer-visibility",
+															function(event,
+																	layerId,
+																	visibility) {
+																if (layerId != portalLayer.id) {
+																	return;
+																}
+
+																if (visibility) {
+																	inlineLegend
+																			.addClass("visible");
+																} else {
+																	inlineLegend
+																			.removeClass("visible");
+																}
+															});
+
+											inlineLegend
+													.click(function() {
+														if ($(
+																"#"
+																		+ portalLayer.id
+																		+ "_visibility_checkbox")
+																.hasClass(
+																		"checked")) {
+															bus
+																	.send(
+																			"open-legend",
+																			wmsLayerWithLegend.id);
+														}
+													});
+
+											tdLegend.append(inlineLegend);
+										}
+									}
+									trLayer.append(tdLegend);
+
+									tdVisibility = $("<td/>").css("width",
+											"16px");
+									divCheckbox = $("<div/>").attr(
+											"id",
+											portalLayer.id
+													+ "_visibility_checkbox")
+											.addClass("layer_visibility");
+									if (portalLayer.active) {
+										divCheckbox.addClass("checked");
+									}
+									divCheckbox
+											.mousedown(
+													function() {
+														divCheckbox
+																.addClass("mousedown");
+													})
+											.mouseup(
+													function() {
+														divCheckbox
+																.removeClass("mousedown");
+													})
+											.mouseenter(function() {
+												divCheckbox.addClass("in");
+											})
+											.mouseleave(function() {
+												divCheckbox.removeClass("in");
+											})
+											.click(
+													function() {
+														divCheckbox
+																.toggleClass("checked");
+														bus
+																.send(
+																		"layer-visibility",
+																		[
+																				portalLayer.id,
+																				divCheckbox
+																						.hasClass("checked") ]);
+													});
+
+									if (!portalLayer.isPlaceholder) {
+										tdVisibility.append(divCheckbox);
+									}
+
+									trLayer.append(tdVisibility);
+
+									tdName = $("<td/>").addClass("layer_name");
+									tdName.html(portalLayer.label);
+									trLayer.append(tdName);
+
+									tdInfo = $("<td/>").addClass("layer_info");
+									if (portalLayer.hasOwnProperty("infoFile")) {
+										aLink = $("<a/>").attr("href",
+												portalLayer.infoFile);
+										aLink.addClass("layer_info_button");
+										aLink.fancybox({
+											"closeBtn" : "true",
+											"openEffect" : "elastic",
+											"closeEffect" : "elastic",
+											"type" : "iframe",
+											"overlayOpacity" : 0.5
+										});
+										tdInfo.append(aLink);
+									}
+									trLayer.append(tdInfo);
+
+									$.each(portalLayer.wmsLayers, function(
+											index, wmsLayer) {
+										if (wmsLayer
+												.hasOwnProperty("timestamps")) {
+											temporalLayers.push(wmsLayer);
+										}
+									});
+
+									tblLayerGroup.append(trLayer);
+									divLayers.accordion("refresh");
+								}
+							});
+
+			bus.listen("layer-visibility", function(event, layerId, visible) {
+				var divCheckbox = $("#" + layerId + "_visibility_checkbox");
+				if (visible) {
+					divCheckbox.addClass("checked");
+				} else {
+					divCheckbox.removeClass("checked");
+				}
 			});
 
-			divTitle.append(infoButton);
-		}
-
-
-		tblLayerGroup = $("<table/>");
-		tblLayerGroup.attr("id", "group-content-table-" + groupInfo.id);
-		
-		if (groupInfo.hasOwnProperty("parentId")) {
-			parentId = groupInfo.parentId;
-			tblParentLayerGroup = $("#group-content-table-" + parentId);
-			if (tblParentLayerGroup.length == 0) {
-				bus.send("error", "Group " + groupInfo.name + " references nonexistent group: " + parentId);
-			}
-			tblParentLayerGroup.append(divTitle).append(tblLayerGroup);
-		} else {
-			divLayers.append(divTitle);
-			divContent = $("<div/>").css("padding", "10px 2px 10px 2px");
-			divContent.append(tblLayerGroup);
-			divLayers.append(divContent).accordion("refresh");
-		}
-	});
-
-	bus.listen("add-layer", function(event, portalLayer) {
-		var tblLayerGroup, trLayer, tdLegend,
-			tdVisibility, divCheckbox, tdName, tdInfo, aLink, inlineLegend;
-
-		tblLayerGroup = $("#group-content-table-" + portalLayer.groupId);
-		if (tblLayerGroup.length == 0) {
-			bus.send("error", "Layer " + portalLayer.label + " references nonexistent group: " + portalLayer.groupId);
-		} else {
-			trLayer = $("<tr/>").attr("id", "layer-row-" + portalLayer.id).addClass("layer_row");
-
-			tdLegend = $("<td/>").addClass("layer_legend");
-
-			if (portalLayer.hasOwnProperty("inlineLegendUrl")) {
-				// context has an inline legend
-				//tdLegend = $('<td style="width:20px">');
-				//inlineLegend = $('<img class="inline-legend" src="' + UNREDD.wmsServers[0] + contextConf.inlineLegendUrl + '">');
-				inlineLegend = $('<img class="inline-legend" src="' + portalLayer.inlineLegendUrl + '">');
-				tdLegend.append(inlineLegend);
-			} else {
-				var wmsLayersWithLegend = portalLayer.wmsLayers.filter(function(layer) {
-				    return layer.hasOwnProperty("legend");
-				});
-				var wmsLayerWithLegend = wmsLayersWithLegend[0];
-
-				if (wmsLayerWithLegend) {
-					inlineLegend = $("<td/>");
-					inlineLegend.addClass("inline-legend-button");
-
-					if (portalLayer.active) {
-						inlineLegend.addClass("visible");
+			bus.listen("time-slider.selection", function(event, date) {
+				for (var i = 0; i < temporalLayers.length; i++) {
+					var layer = temporalLayers[i];
+					var timestamps = layer.timestamps;
+					var closestPrevious = null;
+					timestamps.sort();
+					for (var j = 0; j < timestamps.length; j++) {
+						var timestampString = timestamps[j];
+						var timestamp = new Date();
+						timestamp.setISO8601(timestampString);
+						if (timestamp <= date) {
+							closestPrevious = timestamp;
+						} else {
+							break;
+						}
 					}
 
-					bus.listen("layer-visibility", function(event, layerId, visibility) {
-						if (layerId != portalLayer.id) {
-							return;
-						}
+					if (closestPrevious == null) {
+						closestPrevious = new Date();
+						closestPrevious.setISO8601(timestamps[0]);
+					}
 
-						if (visibility) {
-							inlineLegend.addClass("visible");
-						} else {
-							inlineLegend.removeClass("visible");
-						}
-					});
+					var tdLayerName = $("#layer-row-" + layer.id
+							+ " .layer_name");
+					tdLayerName.find("span").remove();
+					$("<span/>").html(
+							" (" + closestPrevious.getUTCFullYear() + ")")
+							.appendTo(tdLayerName);
 
-					inlineLegend.click(function() {
-						if ($("#" + portalLayer.id + "_visibility_checkbox").hasClass("checked")) {
-							bus.send("open-legend", wmsLayerWithLegend.id);
-						}
-					});
-
-					tdLegend.append(inlineLegend);
-				}
-			}
-			trLayer.append(tdLegend);
-
-			tdVisibility = $("<td/>").css("width", "16px");
-			divCheckbox = $("<div/>").attr("id", portalLayer.id + "_visibility_checkbox").addClass("layer_visibility");
-			if (portalLayer.active) {
-				divCheckbox.addClass("checked");
-			}
-			divCheckbox.mousedown(function() {
-				divCheckbox.addClass("mousedown");
-			}).mouseup(function() {
-				divCheckbox.removeClass("mousedown");
-			}).mouseenter(function() {
-					divCheckbox.addClass("in");
-			}).mouseleave(function() {
-				divCheckbox.removeClass("in");
-			}).click(function() {
-				divCheckbox.toggleClass("checked");
-				bus.send("layer-visibility", [ portalLayer.id, divCheckbox.hasClass("checked") ]);
-			});
-
-			if(!portalLayer.isPlaceholder){
-				tdVisibility.append(divCheckbox);
-			}
-
-			trLayer.append(tdVisibility);
-
-			tdName = $("<td/>").addClass("layer_name");
-			tdName.html(portalLayer.label);
-			trLayer.append(tdName);
-
-			tdInfo = $("<td/>").addClass("layer_info");
-			if (portalLayer.hasOwnProperty("infoFile")) {
-				aLink = $("<a/>").attr("href", portalLayer.infoFile);
-				aLink.addClass("layer_info_button");
-				aLink.fancybox({
-					"closeBtn": "true",
-					"openEffect": "elastic",
-					"closeEffect": "elastic",
-					"type": "iframe",
-					"overlayOpacity": 0.5
-				});
-				tdInfo.append(aLink);
-			}
-			trLayer.append(tdInfo);
-
-			$.each(portalLayer.wmsLayers, function(index, wmsLayer) {
-				if (wmsLayer.hasOwnProperty("timestamps")) {
-					temporalLayers.push(wmsLayer);
+					bus.send("layer-timestamp-selected", [ layer.id,
+							closestPrevious ]);
 				}
 			});
+			bus.listen("layer-time-slider.selection", function(event, layerid,date) {
+				var tdLayerName = $("#layer-row-" + layerid + " .layer_name");
+				tdLayerName.find("span").remove();
+				$("<span/>").html(" (" + date.getUTCFullYear() + ")").appendTo(
+						tdLayerName);
 
-			tblLayerGroup.append(trLayer);
-			divLayers.accordion("refresh");
-		}
-	});
+			});
+			bus.listen("show-layer-list", function(event, groupInfo) {
+				divLayersContainer.show();
+			});
+			bus.listen("hide-layer-list", function(event, groupInfo) {
+				divLayersContainer.hide();
+			});
 
-	bus.listen("layer-visibility", function(event, layerId, visible) {
-		var divCheckbox = $("#" + layerId + "_visibility_checkbox");
-		if (visible) {
-			divCheckbox.addClass("checked");
-		} else {
-			divCheckbox.removeClass("checked");
-		}
-	});
-
-	bus.listen("time-slider.selection", function(event, date) {
-		for (var i = 0; i < temporalLayers.length; i++) {
-			var layer = temporalLayers[i];
-			var timestamps = layer.timestamps;
-			var closestPrevious = null;
-			timestamps.sort();
-			for ( var j = 0; j < timestamps.length; j++) {
-				var timestampString = timestamps[j];
-				var timestamp = new Date();
-				timestamp.setISO8601(timestampString);
-				if (timestamp <= date) {
-					closestPrevious = timestamp;
-				} else {
-					break;
-				}
-			}
-
-			if (closestPrevious == null) {
-				closestPrevious = new Date();
-				closestPrevious.setISO8601(timestamps[0]);
-			}
-
-			var tdLayerName = $("#layer-row-" + layer.id + " .layer_name");
-			tdLayerName.find("span").remove();
-			$("<span/>").html(" (" + closestPrevious.getUTCFullYear() + ")").appendTo(tdLayerName);
-
-			bus.send("layer-timestamp-selected", [layer.id, closestPrevious]);
-		}
-	});
-
-	bus.listen("show-layer-list", function(event, groupInfo) {
-		divLayersContainer.show();
-	});
-	bus.listen("hide-layer-list", function(event, groupInfo) {
-		divLayersContainer.hide();
-	});
-
-	return divLayersContainer;
-});
+			return divLayersContainer;
+		});
---
+++ src/main/webapp/modules/layer-time-sliders.js
@@ -0,0 +1,94 @@
+define([ "jquery", "message-bus", "layout", "botonera", "map", "jquery-ui" ], function($, bus, layout, botonera, map) {
+    var aLayers=[];
+    var aTimestampsLayers={};
+	var divTimeSliders = $("<div/>").attr("id", "layerTimeSliders");
+
+	botonera.newButton("temporal", function() {
+		divTimeSliders.dialog({
+			closeOnEscape : true,
+			width : "80%",
+			resizable : true
+		});
+	});
+
+	bus.listen("add-layer", function(event, layerInfo) {
+		var timestamps = [];
+		$.each(layerInfo.wmsLayers, function(index, wmsLayer) {
+			if (wmsLayer.hasOwnProperty("timestamps")) {
+				for (var i = 0; i < wmsLayer.timestamps.length; i++) {
+					var d = new Date();
+					d.setISO8601(wmsLayer.timestamps[i]);
+					timestamps.push(d);
+				}
+			}
+		});
+
+		if (timestamps.length > 0) {
+			timestamps.sort(function(a, b) {
+				return a - b;
+			});
+
+			$("<div/>").html(layerInfo.label).appendTo(divTimeSliders);
+			var divTimeSlider = $("<div id='layer_time_slider_" + layerInfo.id + "' class='layers_time_slider' />").appendTo(divTimeSliders);
+
+			var divTimeSliderLabel = $("<div id='layer_time_slider_label_" + layerInfo.id + "'/>").appendTo(divTimeSliders);
+
+			divTimeSlider.slider({
+				change : function(event, ui) {
+					if (event.originalEvent) {
+						var date = timestamps[ui.value];
+						$.each(layerInfo.wmsLayers, function(index, wmsLayer) {
+							var layer = map.getLayer(wmsLayer.id);
+							layer.mergeNewParams({
+								'time' : date.toISO8601String()
+							});
+							bus.send("layer-time-slider.selection", [layerInfo.id,date]);
+						});
+					}else{ //Programatic change
+						//alert('programatic');
+					};
+					
+				},
+				slide : function(event, ui) {
+					var date = timestamps[ui.value];
+					divTimeSliderLabel.text(date);
+				},
+				max : timestamps.length - 1,
+				value : timestamps.length - 1
+			});
+
+			divTimeSliderLabel.text(timestamps[timestamps.length - 1]);
+		
+		   aTimestampsLayers[layerInfo.id]=timestamps;
+		}
+
+	});
+	
+	bus.listen("time-slider.selection",function(obj,d){
+
+		console.log(aTimestampsLayers);
+		$.each(aTimestampsLayers, function(layerid,steps){
+			var position_i=-1,position_min=-1,position_max=-1;
+			console.log(layerid);
+			$.each(steps,function(position,date_value){
+				if (date_value.valueOf()==d.valueOf()) {
+					position_i=position;
+					
+				} else if (date_value.valueOf()<d.valueOf()) {
+					position_min=position;
+					//console.log(date_value+' menor');
+				}else{
+					if (position_max==-1) {position_max=position;};
+					//console.log(date_value+' mayor');
+				};
+				});
+			console.log(layerid+' -> '+position_i+', '+position_min+', '+position_max);
+			var pos;
+			if (position_i>-1)
+				{pos=position_i;}
+			else{ pos=position_min;}
+			$('#layer_time_slider_'+layerid).slider('value',pos);
+		}
+		);
+	});
+});
