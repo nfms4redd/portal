61f7bd23bdbdffe3ba6c3e6ec6fa8cd893354fff
Fernando González <fernando.gonzalez@geomati.co>
2014-11-19 09:22:59 +0100

    layer time sliders in its own plugin project

--- layer-time-sliders/src/main/resources/nfms/modules/botonera.js
+++ layer-time-sliders/src/main/resources/nfms/modules/botonera.js
@@ -1,18 +1,16 @@
-define([ "jquery", "layout" ], function($, layout) {
-	var botonera = layout.toolbar; //layerTimeBoton; //$("<div/>").attr("style", "position:absolute;top:0; left:7em; z-index:2000");
-	//botonera.appendTo(layout["map"]);
+define([ "jquery", "toolbar" ], function($, toolbar) {
 
 	return {
 		newButton : function(text, callback) {
 			var aButton = $("<a/>").html(text);
 			aButton.addClass("blue_button").addClass("lang_button");
-			aButton.appendTo(botonera);
+			aButton.appendTo(toolbar);
 			aButton.click(callback);
 		},
 		newText : function(callback) {
 			var inputText = $("<input/>").attr("type", "text").attr("size", "15");
 			inputText.attr("style", "margin-top:0.9em;margin-left:3em");
-			inputText.appendTo(botonera);
+			inputText.appendTo(toolbar);
 			inputText.keypress(function(event) {
 				if (event.which == 13) {
 					callback(inputText.val());
