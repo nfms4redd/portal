93d79a0474446a4efc1660000b6b3984b9c58823
Fernando González <fernando.gonzalez@geomati.co>
2014-12-10 16:08:54 +0100

    Provide more information in the error messages to the user

--- core/src/main/java/org/fao/unredd/portal/ErrorFilter.java
+++ core/src/main/java/org/fao/unredd/portal/ErrorFilter.java
@@ -1,6 +1,7 @@
 package org.fao.unredd.portal;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -9,6 +10,8 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONSerializer;
 
 import org.apache.log4j.Logger;
 
@@ -25,23 +28,28 @@ public class ErrorFilter implements Filter {
 			FilterChain chain) throws IOException, ServletException {
 		try {
 			chain.doFilter(request, response);
-		} catch (Exception e) {
+		} catch (Throwable e) {
 			logger.error("Error handling request", e);
 
-			String errorMsg = "Server error";
+			String errorMsg = "Server error: ";
 			HttpServletResponse httpResponse = (HttpServletResponse) response;
 			if (e instanceof StatusServletException) {
 				httpResponse
 						.setStatus(((StatusServletException) e).getStatus());
-				errorMsg = e.getMessage();
 			} else {
 				httpResponse.setStatus(500);
+			}
+			while (e != null) {
+				errorMsg += e.getMessage() + ". ";
+				e = e.getCause();
 			}
 			response.setContentType("application/json");
 			response.setCharacterEncoding("utf8");
 			if (errorMsg != null) {
-				String msg = "{\"message\":\"" + errorMsg + "\"}";
-				response.getOutputStream().write(msg.getBytes());
+				HashMap<String, String> doc = new HashMap<String, String>();
+				doc.put("message", errorMsg);
+				response.getOutputStream().write(
+						JSONSerializer.toJSON(doc).toString(0).getBytes());
 			}
 		}
 	}
