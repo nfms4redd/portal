b2db0b0425b3be4bfb7738ebcbb58031ff2625d8
Fernando González <fernando.gonzalez@geomati.co>
2014-12-22 10:19:50 +0100

    banner in two pieces

--- base/src/main/resources/nfms/modules/banner.js
+++ base/src/main/resources/nfms/modules/banner.js
@@ -1,7 +1,7 @@
 define([ "jquery", "layout", "i18n", "message-bus", "module" ], function($, layout, i18n, bus, module) {
 
 	var config = module.config();
-	
+
 	if (!config["hide"]) {
 		var divBanner = $("<div/>").attr("id", "banner");
 		layout.header.prepend(divBanner);
@@ -9,7 +9,10 @@ define([ "jquery", "layout", "i18n", "message-bus", "module" ], function($, layo
 		if (config["show-flag"]) {
 			$("<div/>").attr("id", "flag").appendTo(divBanner);
 		}
-		$("<div/>").attr("id", "logos").appendTo(divBanner);
+		if (config["show-logos"]) {
+			$("<div/>").attr("id", "logos").appendTo(divBanner);
+		}
 		$("<span/>").attr("id", "title").html(i18n["title"]).appendTo(divBanner);
+		$("<div/>").attr("id", "banner-izq").appendTo(divBanner);
 	}
 });
