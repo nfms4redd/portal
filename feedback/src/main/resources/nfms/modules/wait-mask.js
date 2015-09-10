28807718f9591a1c64ff3f0db10fd4da21ff619a
Fernando González Cortés <fernando.gonzalez@geomati.co>
2015-09-10 09:49:51 +0200

    wait-mask event decomposed in show and hide events

--- feedback/src/main/resources/nfms/modules/wait-mask.js
+++ feedback/src/main/resources/nfms/modules/wait-mask.js
@@ -1,17 +1,15 @@
 define(["message-bus", "jquery"], function(bus, $) {
 
-	bus.listen("wait-mask", function(event, message) {
-
-		if (message) {
+	bus.listen("show-wait-mask", function(event, message) {
 			// Display a message, blocking the UI
 			var background = $('<div/>').addClass("wait-mask").appendTo($("body"));
 			var center = $('<div/>').appendTo(background);
 			$('<div/>').html(message).appendTo(center);
-		} else {
-			// Remove the wait-mask
-			$('.wait-mask').remove();
-		}
-
+	});
+	
+	bus.listen("hide-wait-mask", function(event) {
+		// Remove the wait-mask
+		$('.wait-mask').remove();
 	});
 
 });
