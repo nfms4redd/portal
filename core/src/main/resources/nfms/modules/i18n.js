afdeea351391e63e2cd06ca5ea34cb7a924f2ade
Fernando González Cortés <fernando.gonzalez@geomati.co>
2015-11-16 15:45:33 +0100

    Don't show <br/> in title

--- core/src/main/resources/nfms/modules/i18n.js
+++ core/src/main/resources/nfms/modules/i18n.js
@@ -1,7 +1,7 @@
 define([ "module" ], function(module) {
-	
+
 	var translations = module.config();
-	document.title = translations["title"];
-	
+	document.title = translations["title"].replace("<br/>", " ");
+
 	return translations;
 });
