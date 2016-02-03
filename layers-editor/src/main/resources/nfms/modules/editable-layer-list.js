29c3dcaa11fefd3eb49a1c4fea18ff1f315402e4
Fernando González Cortés <fernando.gonzalez@geomati.co>
2016-02-03 17:03:31 +0100

    Formated code

--- layers-editor/src/main/resources/nfms/modules/editable-layer-list.js
+++ layers-editor/src/main/resources/nfms/modules/editable-layer-list.js
@@ -1,4 +1,4 @@
-define(["message-bus", "layers-edit-form", "layers", "layers-json", "jquery", "jquery-ui"], function(bus, forms, layers, layers_json, $) {
+define([ "message-bus", "layers-edit-form", "layers", "layers-json", "jquery", "jquery-ui" ], function(bus, forms, layers, layers_json, $) {
 
 	bus.listen("before-adding-layers", function() {
 		bus.send("register-layer-action", function(layer) {
@@ -14,7 +14,7 @@ define(["message-bus", "layers-edit-form", "layers", "layers-json", "jquery", "j
 		});
 		bus.send("register-layer-action", function(layer) {
 			return $("<a/>").addClass("editable-layer-list-button").addClass("layer_deleteLayer_button").click(function() {
-				layers_json.deleteLayer(layer.getId(), function(){
+				layers_json.deleteLayer(layer.getId(), function() {
 					layers.redraw(layers_json.root);
 				});
 			});
@@ -36,14 +36,15 @@ define(["message-bus", "layers-edit-form", "layers", "layers-json", "jquery", "j
 	}
 
 	bus.listen("layers-loaded", function() {
-		//var add = $("<div/>").addClass("root_group_placeholder").append("Add Group ").append($("<div/>").addClass("fa fa-plus")); // TODO i18n
-		//$("#layers_container").append(add);
+		// var add = $("<div/>").addClass("root_group_placeholder").append("Add
+		// Group ").append($("<div/>").addClass("fa fa-plus")); // TODO i18n
+		// $("#layers_container").append(add);
 
 		// Añadir placeholder para soltar subgrupos
 		$(".group").each(function(i, el) {
 			var groupInfo = getGroups($(el));
 			if (groupInfo.items.length == 0) { // Sólo en grupos vacíos
-				$("<div/>").addClass("group_placeholder").addClass("group-container").appendTo($("#group-content-table-"+groupInfo.id));
+				$("<div/>").addClass("group_placeholder").addClass("group-container").appendTo($("#group-content-table-" + groupInfo.id));
 			}
 		});
 
@@ -51,13 +52,13 @@ define(["message-bus", "layers-edit-form", "layers", "layers-json", "jquery", "j
 		$("<tr/>").addClass("layer_placeholder").appendTo(".layer-container");
 
 		$(".group-container").sortable({
-			handle: ".group-title",
-			connectWith: ".group-container",
-			axis: "y",
-			cursor: "move",
-			placeholder: "root_group_placeholder",
-			forcePlaceholderSize: true,
-			stop: function(event, ui) {
+			handle : ".group-title",
+			connectWith : ".group-container",
+			axis : "y",
+			cursor : "move",
+			placeholder : "root_group_placeholder",
+			forcePlaceholderSize : true,
+			stop : function(event, ui) {
 				// IE doesn't register the blur when sorting
 				// so trigger focusout handlers to remove .ui-state-focus
 				ui.item.children(".ui-accordion-header").triggerHandler("focusout");
@@ -74,20 +75,20 @@ define(["message-bus", "layers-edit-form", "layers", "layers-json", "jquery", "j
 
 		$("#all_layers").accordion("destroy");
 		$("#all_layers > div").accordion({
-			animate: false,
-			header: " > div.group-title",
-			heightStyle: "content",
-			collapsible: true,
-			active: false
+			animate : false,
+			header : " > div.group-title",
+			heightStyle : "content",
+			collapsible : true,
+			active : false
 		});
 
 		$(".layer-container").sortable({
-			connectWith: ".layer-container",
-			axis: "y",
-			cursor: "move",
-			placeholder: "root_group_placeholder",
-			forcePlaceholderSize: true,
-			stop: function(event, ui) {
+			connectWith : ".layer-container",
+			axis : "y",
+			cursor : "move",
+			placeholder : "root_group_placeholder",
+			forcePlaceholderSize : true,
+			stop : function(event, ui) {
 				save();
 			}
 		});
@@ -95,7 +96,7 @@ define(["message-bus", "layers-edit-form", "layers", "layers-json", "jquery", "j
 
 	function getGroups(el) {
 		var attrs = el.attr("data-group") ? JSON.parse(el.attr("data-group")) : {};
-		if(attrs.hasOwnProperty("name")) {
+		if (attrs.hasOwnProperty("name")) {
 			attrs.label = attrs.name;
 		}
 		delete attrs.name;
@@ -111,9 +112,9 @@ define(["message-bus", "layers-edit-form", "layers", "layers-json", "jquery", "j
 	function getLayers(el) {
 		var ids = [];
 		el.find(".layer_row").filter(function() {
-			return $(this).parentsUntil(el,'.group').length < 1;
+			return $(this).parentsUntil(el, '.group').length < 1;
 		}).each(function(i) {
-			var id = $(this).attr('id').replace('layer-row-','');
+			var id = $(this).attr('id').replace('layer-row-', '');
 			ids.push(id);
 		});
 		return ids;
--- layers-editor/src/main/resources/nfms/modules/layers-json.js
+++ layers-editor/src/main/resources/nfms/modules/layers-json.js
@@ -1,4 +1,4 @@
-define(["text!../layers.json", "message-bus"], function(layersjson, bus) {
+define([ "text!../layers.json", "message-bus" ], function(layersjson, bus) {
 
 	var config = JSON.parse(layersjson);
 
@@ -46,7 +46,7 @@ define(["text!../layers.json", "message-bus"], function(layersjson, bus) {
 		copy(wmsLayer, wmsLayerInput);
 
 		var portalLayer = getPortalLayer(portalLayerInput.id);
-		copy(portalLayer,  portalLayerInput);
+		copy(portalLayer, portalLayerInput);
 
 		upload(callback);
 	}
@@ -59,12 +59,12 @@ define(["text!../layers.json", "message-bus"], function(layersjson, bus) {
 
 		upload(callback);
 	}
-	
+
 	function deleteLayer(layerId, callback) {
 		for (var i = 0; i < config.portalLayers.length; i++) {
 			var portalLayer = config.portalLayers[i];
 			if (portalLayer.id == layerId) {
-				config.portalLayers.splice(i ,1);
+				config.portalLayers.splice(i, 1);
 				for (var j = 0; j < portalLayer.layers.length; j++) {
 					var wmsLayerId = portalLayer.layers[j];
 					for (var k = 0; k < config.wmsLayers.length; k++) {
@@ -88,13 +88,13 @@ define(["text!../layers.json", "message-bus"], function(layersjson, bus) {
 
 	function copy(target, source) {
 		// Copy over new values
-		for (var name in source) {
+		for ( var name in source) {
 			target[name] = source[name];
 		}
 
 		// Delete all properties not in source
-		for (var key in target) {
-			if(!source.hasOwnProperty(key)) {
+		for ( var key in target) {
+			if (!source.hasOwnProperty(key)) {
 				delete target[key];
 			}
 		}
@@ -107,7 +107,7 @@ define(["text!../layers.json", "message-bus"], function(layersjson, bus) {
 			return el.hasOwnProperty(key) && el[key] == value;
 		}, recurse);
 	}
-	
+
 	function find(arr, test, recurse) {
 		for (var i = 0; i < arr.length; i++) {
 			var el = arr[i];
@@ -124,31 +124,31 @@ define(["text!../layers.json", "message-bus"], function(layersjson, bus) {
 
 	function upload(onSuccess) {
 		bus.send("ajax", {
-			type: 'PUT',
-			url: 'layers.json',
-			contentType: "application/json; charset=utf-8",
-			data: JSON.stringify(config, null, 4),
-			success: function(data, textStatus, jqXHR) {
+			type : 'PUT',
+			url : 'layers.json',
+			contentType : "application/json; charset=utf-8",
+			data : JSON.stringify(config, null, 4),
+			success : function(data, textStatus, jqXHR) {
 				if (onSuccess) {
 					onSuccess.call(null, config);
 				}
 			},
-			errorMsg: "Error uploading layers.json to the server" // TODO i18n
+			errorMsg : "Error uploading layers.json to the server" // TODO i18n
 		});
 	}
 
 	return {
-		getServer: getServer,
-		getPortalLayer: getPortalLayer,
-		getWmsLayer: getWmsLayer,
-		getGroup: getGroup,
-		updateServer: updateServer,
-		updateGroup: updateGroup,
-		updateLayer: updateLayer,
-		deleteLayer: deleteLayer,
-		addNewLayer: addNewLayer,
-		updateGroups: updateGroups,
-		getGroups: getGroups,
-		root: config
+		getServer : getServer,
+		getPortalLayer : getPortalLayer,
+		getWmsLayer : getWmsLayer,
+		getGroup : getGroup,
+		updateServer : updateServer,
+		updateGroup : updateGroup,
+		updateLayer : updateLayer,
+		deleteLayer : deleteLayer,
+		addNewLayer : addNewLayer,
+		updateGroups : updateGroups,
+		getGroups : getGroups,
+		root : config
 	};
 });
