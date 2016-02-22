d782cf7955cbca09e4b18ecf7d6d0fed13610155
Fernando González Cortés <fernando.gonzalez@geomati.co>
2016-02-22 15:49:06 +0100

    Squire to mock requirejs modules

--- core/src/test/resources/nfms/modules/layersTest.js
+++ core/src/test/resources/nfms/modules/layersTest.js
@@ -1,42 +1,52 @@
 describe("board tests", function() {
 
+	var injector;
 	var bus;
 	var customization;
 	var module;
 
 	beforeEach(function(done) {
-		customization = {
-			"languageCode" : "es"
-		};
-		module = {
-			"config" : function() {
-				var layersJson = {
-					"default-server" : "http://demo1.geo-solutions.it",
-					"wmsLayers" : [],
-					"portalLayers" : [],
-					"groups" : []
-				};
-				return layersJson;
-			}
-		};
-
 		require.config({
 			"baseUrl" : "src/",
 			"paths" : {
 				"jquery" : "/jslib/jquery-2.1.0"
+			},
+			"config" : {
+				"layers" : {
+					"default-server" : "http://demo1.geo-solutions.it",
+					"wmsLayers" : [],
+					"portalLayers" : [],
+					"groups" : []
+				}
 			}
 		});
-		require([ "message-bus" ], function(loadedModule) {
-			bus = loadedModule;
-			done();
+		require([ "/test-jslib/Squire.js" ], function(Squire) {
+			if (injector != null) {
+				injector.clean();
+				injector.remove();
+			}
+			injector = new Squire();
+			injector.require([ "message-bus" ], function(loadedModule) {
+				bus = loadedModule;
+				done();
+			});
 		});
 	});
 
 	it("layers init", function(done) {
 		spyOn(bus, "listen");
-		require(["layers"], function(){
+		injector.require([ "layers" ], function() {
 			expect(bus.listen).toHaveBeenCalled();
 			done();
+		});
+	});
+
+	it("layers process", function(done) {
+		injector.require([ "layers" ], function() {
+			bus.listen("layers-loaded", function(e, layerRoot) {
+				done();
+			});
+			bus.send("modules-loaded");
 		});
 	});
 });
